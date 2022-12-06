b13ede831e69999878a4801783fd391ebdd9cff2
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-06 16:40:51 +0900

    2022.12.06 problem update

---
+++ problems/[221206] Odd Even Linked List.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * leetcode problem link: https://leetcode.com/problems/odd-even-linked-list/
+ *
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const oddEvenList = function(head) {
+
+};
