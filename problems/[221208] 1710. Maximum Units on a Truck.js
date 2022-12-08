5ac845897427d9e22ddce42e744c76bd8e0a2780
Jimoon <roadzmoon76@gmail.com>
2022-12-08 11:34:55 +0900

    [2022.12.08] 길지문

--- problems/[221208] 1710. Maximum Units on a Truck.js
+++ problems/[221208] 1710. Maximum Units on a Truck.js
@@ -5,6 +5,19 @@
  * @param {number} truckSize
  * @return {number}
  */
- const maximumUnits = function(boxTypes, truckSize) {
+const maximumUnits = function(boxTypes, truckSize) {
+  const sortedBoxTypes = [...boxTypes];
+  sortedBoxTypes.sort((a, b) => a[1] - b[1]);
+  let result = 0;
 
- };
+  while (truckSize && sortedBoxTypes.length) {
+      const currentBox = sortedBoxTypes.pop();
+
+      if (currentBox[0] > truckSize) return result += (currentBox[1] * truckSize);
+
+      result += (currentBox[1] * currentBox[0]);
+      truckSize -= currentBox[0];
+  }
+
+    return result;
+};
