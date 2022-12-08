10b2ee6f29cc36fec8b30e3bbf61887b9fe5fcae
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-08 20:15:04 +0900

    fix conflict

--- problems/[221208] 1710. Maximum Units on a Truck.js
+++ problems/[221208] 1710. Maximum Units on a Truck.js
@@ -6,5 +6,26 @@
  * @return {number}
  */
 const maximumUnits = function (boxTypes, truckSize) {
+  return boxTypes
+    .slice()
+    .sort((a, b) => b[1] - a[1])
+    .reduce((result, currentType) => {
+      if (result.leftBoxCount === 0) {
+        return result;
+      }
 
+      const [numberOfBoxes, numberOfUnitsPerBox] = currentType;
+      const enableBoxCount = result.leftBoxCount >= numberOfBoxes
+        ? numberOfBoxes
+        : result.leftBoxCount;
+
+      return {
+        leftBoxCount: result.leftBoxCount - enableBoxCount,
+        total: result.total + enableBoxCount * numberOfUnitsPerBox
+      };
+    }, {
+      leftBoxCount: truckSize,
+      total: 0
+    })
+    .total;
 };
