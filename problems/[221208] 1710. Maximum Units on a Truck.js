f0d78bca5153432bc98938290ee59a81e20e85a3
Jimoon <roadzmoon76@gmail.com>
2022-12-08 23:17:34 +0900

    Add 221208 solution

--- problems/[221208] 1710. Maximum Units on a Truck.js
+++ problems/[221208] 1710. Maximum Units on a Truck.js
@@ -6,5 +6,17 @@
  * @return {number}
  */
 const maximumUnits = function (boxTypes, truckSize) {
+  const sortedBoxTypes = [...boxTypes].sort((a, b) => b[1] - a[1]);
+  let result = 0;
 
+  for (const box of sortedBoxTypes) {
+    const boxCount = Math.min(box[0], truckSize);
+
+    result += box[1] * boxCount;
+    truckSize -= boxCount;
+
+    if (!truckSize) break;
+  }
+
+  return result;
 };
