1cbd8481aa3000be053495a21f8f889b1a3bf0c0
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-08 11:53:36 +0900

    mission complete

--- problems/[221208] 1710. Maximum Units on a Truck.js
+++ problems/[221208] 1710. Maximum Units on a Truck.js
@@ -5,6 +5,24 @@
  * @param {number} truckSize
  * @return {number}
  */
- const maximumUnits = function(boxTypes, truckSize) {
+const maximumUnits = function (boxTypes, truckSize) {
+  let copyTruckSize = truckSize;
 
- };
+  return boxTypes
+    .sort((a, b) => b[1] - a[1])
+    .reduce((acc, [numberOfBoxes, numberOfUnits]) => {
+      let copyBoxCount = numberOfBoxes;
+
+      for (let i = 0; i < copyBoxCount; i++) {
+        if (copyTruckSize > 0) {
+          numberOfBoxes -= 1;
+          acc += numberOfUnits;
+          copyTruckSize -= 1;
+        } else {
+          break;
+        }
+      }
+
+      return acc;
+    }, 0);
+};
