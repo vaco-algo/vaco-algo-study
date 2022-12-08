1ab269fe7a94f62db8b272f618c4ea079c34abdc
eunhye210 <tkrhd0210@gmail.com>
2022-12-08 11:33:23 +0900

    221208 algo

--- problems/[221208] 1710. Maximum Units on a Truck.js
+++ problems/[221208] 1710. Maximum Units on a Truck.js
@@ -5,6 +5,24 @@
  * @param {number} truckSize
  * @return {number}
  */
- const maximumUnits = function(boxTypes, truckSize) {
+const maximumUnits = function (boxTypes, truckSize) {
+  let boxArray = boxTypes;
+  let totalSize = truckSize;
+  let answer = 0;
 
- };
+  if (totalSize <= 0) return answer;
+
+  boxArray.sort((a, b) => b[1] - a[1]);
+
+  for (const box of boxArray) {
+    if (totalSize >= box[0]) {
+      answer += box[0] * box[1];
+      totalSize -= box[0];
+    } else {
+      answer += totalSize * box[1];
+      break;
+    }
+  }
+
+  return answer;
+};
