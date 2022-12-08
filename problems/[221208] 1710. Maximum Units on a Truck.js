318b48cd77a52d1b59b2ae7811e05ad5d5331038
Areum Han <hanryu1109@gmail.com>
2022-12-08 23:48:40 +0900

    Maximun Units on a Truck 문제 1차 풀이 완료

--- problems/[221208] 1710. Maximum Units on a Truck.js
+++ problems/[221208] 1710. Maximum Units on a Truck.js
@@ -6,5 +6,17 @@
  * @return {number}
  */
 const maximumUnits = function (boxTypes, truckSize) {
+  boxTypes.sort((a, b) => b[1] - a[1]);
 
+  let answer = 0;
+  for (const [count, units] of boxTypes) {
+    const boxesCount = Math.min(count, truckSize);
+
+    answer += boxesCount * units;
+    truckSize -= boxesCount;
+
+    if (truckSize === 0) break;
+  }
+
+  return answer;
 };
