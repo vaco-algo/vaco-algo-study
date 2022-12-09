875406e25a2615fd112dd2ee3dc42567d86f4c1e
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-09 11:23:24 +0900

    test

--- problems/[221209] 2078. Two Furthest Houses With Different Colors.js
+++ problems/[221209] 2078. Two Furthest Houses With Different Colors.js
@@ -5,5 +5,14 @@
  * @return {number}
  */
 var maxDistance = function (colors) {
+  if (new Set(colors).size === 1) return 0;
+  const colorStartEndIndex = {};
 
+  colors.forEach((color, index) => {
+      if (obj[color]) {
+          return obj[color].end = index;
+      }
+
+      obj[color] = { start: index, end: index }
+  })
 };
