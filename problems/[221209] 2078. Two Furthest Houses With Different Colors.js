4597db61b9f1efe09b99ce3ace7e80622cd0086b
tpdud406 <tpdud406@naver.com>
2022-12-09 17:16:33 +0900

    221209 maxDistance

--- problems/[221209] 2078. Two Furthest Houses With Different Colors.js
+++ problems/[221209] 2078. Two Furthest Houses With Different Colors.js
@@ -4,6 +4,43 @@
  * @param {number[]} colors
  * @return {number}
  */
-var maxDistance = function (colors) {
+const maxDistance = function (colors) {
+  let leftPointer = 0;
+  let rightPointer = colors.length;
+  let distance1 = 0;
+  let distance2 = 0;
 
+  if (rightPointer === 1 && colors[leftPointer] !== colors[rightPointer - 1]) {
+    return rightPointer;
+  }
+
+  while (leftPointer !== rightPointer) {
+    if (
+      colors[leftPointer] !== colors[rightPointer - 1] &&
+      distance1 < rightPointer - leftPointer
+    ) {
+      rightPointer -= 1;
+      distance1 = rightPointer - leftPointer;
+      leftPointer += 1;
+    } else {
+      rightPointer -= 1;
+    }
+  }
+
+  leftPointer = 0;
+  rightPointer = colors.length;
+  while (leftPointer !== rightPointer) {
+    if (
+      colors[leftPointer] !== colors[rightPointer - 1] &&
+      distance2 < rightPointer - leftPointer
+    ) {
+      leftPointer -= 1;
+      distance2 = rightPointer - leftPointer;
+      rightPointer += 1;
+    } else {
+      leftPointer -= 1;
+    }
+  }
+
+  return distance1 - distance2 ? distance1 : distance2;
 };
