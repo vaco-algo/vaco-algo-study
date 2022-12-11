939a11fb038c3cd6a4f1873dead6cf0440f6811d
csy <csl8804@gmail.com>
2022-12-11 15:14:16 +0900

    feat: [221211]561.Array Partition 풀이

--- problems/[221211] 561. Array Partition.js
+++ problems/[221211] 561. Array Partition.js
@@ -4,6 +4,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var arrayPairSum = function(nums) {
+var arrayPairSum = function (nums) {
+  const copyNums = [...nums];
 
+  return copyNums
+    .sort((a, b) => {
+      return a - b;
+    })
+    .filter((num, idx) => {
+      return idx % 2 === 0;
+    })
+    .reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
 };
