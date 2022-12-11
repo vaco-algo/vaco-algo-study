af6bb85f17c2bc64c0ebc6fb017d724eba72f2a4
Jimoon <roadzmoon76@gmail.com>
2022-12-11 11:15:35 +0900

    [2022.12.11] 길지문

--- problems/[221211] 561. Array Partition.js
+++ problems/[221211] 561. Array Partition.js
@@ -5,5 +5,12 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
+  const sortedNums = [...nums].sort((a, b) => a - b);
+  let result = 0;
 
+  for (let i = 0; i < sortedNums.length; i += 2) {
+      result += sortedNums[i];
+  }
+
+  return result;
 };
