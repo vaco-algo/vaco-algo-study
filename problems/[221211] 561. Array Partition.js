33d941750d6fef2beb15f37ef8e78bd202a22cfb
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-11 13:26:26 +0900

    mission complete

--- problems/[221211] 561. Array Partition.js
+++ problems/[221211] 561. Array Partition.js
@@ -4,6 +4,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var arrayPairSum = function(nums) {
+var arrayPairSum = function (nums) {
+  const sort = [...nums].sort((a, b) => a - b);
 
+  return sort.reduce((sum, cur, index) => {
+    if (index % 2 !== 0) {
+      return sum;
+    }
+
+    return sum + cur;
+  }, 0);
 };
