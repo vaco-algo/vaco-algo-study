2d8b012dce0c30f1de9dcd78ae2da32a9e9484cf
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-11 13:01:28 +0900

    solve problem

--- problems/[221211] 561. Array Partition.js
+++ problems/[221211] 561. Array Partition.js
@@ -5,5 +5,9 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
-
+  return nums
+    .slice()
+    .sort((a, b) => b - a)
+    .filter((_, idx) => idx % 2)
+    .reduce((acc, num) => acc + num);
 };
