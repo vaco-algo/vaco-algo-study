83e2c3d2df3470d1acfe34b780c32f4d3009c1c3
csy <csl8804@gmail.com>
2022-12-13 11:31:57 +0900

    feat: 11. Container With Most Water 풀이

--- problems/[221213] 11. Container With Most Water.js
+++ problems/[221213] 11. Container With Most Water.js
@@ -5,5 +5,16 @@
  * @return {number}
  */
 const maxArea = function(height) {
+  const result = [];
 
+  for (let i = 0; i < height.length; i++) {
+    for (let j = 1; j < height.length; j++) {
+      const horizontal = Math.abs(j - i);
+      const vertical = Math.min(height[j], height[i]);
+
+      const area = horizontal * vertical;
+      result.push(area);
+    }
+  }
+  return Math.max(...result);
 };
