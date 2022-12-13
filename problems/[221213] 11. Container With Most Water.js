cb31144c435a242ef5a8cbfca0ab00599e0dad09
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-13 11:48:40 +0900

    upload problem

--- problems/[221213] 11. Container With Most Water.js
+++ problems/[221213] 11. Container With Most Water.js
@@ -5,5 +5,31 @@
  * @return {number}
  */
 const maxArea = function(height) {
+  const max = {
+    value: null,
+    index: null
+  };
 
+  let result = 0;
+
+  for (let i = 0; i < height.length; i++) {
+    if (height[i] > max.value) {
+      max.value = height[i];
+      max.index = [i];
+    } else if (height[i] === max.value) {
+      max.index.push(i);
+    }
+  }
+
+  for (let j = 0; j < height.length; j++) {
+    for (let k = 0; k < max.index.length; k++) {
+      const area = Math.abs((max.index[k] - j) * height[j]);
+
+      if (area > result) {
+        result = area;
+      }
+    }
+  }
+
+  return result;
 };
