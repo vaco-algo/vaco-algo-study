de8bc2b560204599f6712b5b23944df41b3a07ae
Jimoon <roadzmoon76@gmail.com>
2022-12-13 12:07:31 +0900

    [2022.12.13] 길지문

--- problems/[221213] 11. Container With Most Water.js
+++ problems/[221213] 11. Container With Most Water.js
@@ -5,5 +5,26 @@
  * @return {number}
  */
 const maxArea = function(height) {
+  // let mostWater = 0;
 
+  //   for (let i = 0; i < height.length - 1; i++) {
+  //     for (let j = i + 1; j < height.length; j++) {
+  //       mostWater = Math.max(mostWater, (j - i) * Math.min(height[i], height[j]));
+  //     }
+  //   }
+
+  //   return mostWater;
+
+  let start = 0;
+  let end = height.length - 1;
+
+  for (let i = start + 1; i < height.length; i++) {
+    if ((height[i] - height[start]) > i - start) start = i;
+  }
+
+  for (let i = end - 1; i >= 0; i--) {
+    if ((height[i] - height[end]) > end - i) end = i;
+  }
+
+   return (end - start) * Math.min(height[start], height[end]);
 };
