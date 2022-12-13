45fae361f15bbb3bd9b3fe4c7882381e30693562
eunhye210 <tkrhd0210@gmail.com>
2022-12-13 11:36:30 +0900

    221213 algo

--- problems/[221213] 11. Container With Most Water.js
+++ problems/[221213] 11. Container With Most Water.js
@@ -4,6 +4,26 @@
  * @param {number[]} height
  * @return {number}
  */
-const maxArea = function(height) {
+const maxArea = function (height) {
+  let answer = 0;
 
+  for (let i = 0; i < height.length - 1; i++) {
+    for (let j = height.length - 1; j > i; j--) {
+      if (height[i] <= height[j]) {
+        answer = Math.max(answer, height[i] * (j - i));
+        break;
+      }
+    }
+  }
+
+  for (let i = height.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (height[i] <= height[j]) {
+        answer = Math.max(answer, height[i] * (i - j));
+        break;
+      }
+    }
+  }
+
+  return answer;
 };
