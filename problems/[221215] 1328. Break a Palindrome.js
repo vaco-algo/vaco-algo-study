fa2de97934841a7e2da98e8f378f1398d4785902
eunhye210 <tkrhd0210@gmail.com>
2022-12-15 11:41:31 +0900

    221215 algo

--- problems/[221215] 1328. Break a Palindrome.js
+++ problems/[221215] 1328. Break a Palindrome.js
@@ -4,6 +4,21 @@
  * @param {string} palindrome
  * @return {string}
  */
-const breakPalindrome = function (palindrome) {
+ const breakPalindrome = function (palindrome) {
+  const inputString = palindrome.split("");
 
+  if (inputString.length === 1) return "";
+
+  for (let i = 0; i < inputString.length; i++) {
+    if (inputString[i] !== "a" && i !== Math.floor(inputString.length / 2)) {
+      inputString[i] = "a";
+      break;
+    }
+
+    if (i === inputString.length - 1) {
+      inputString[i] = "b";
+    }
+  }
+
+  return inputString.join("");
 };
