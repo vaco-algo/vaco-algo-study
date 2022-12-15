e417652dbd8656d71ef5b09db8720eb7e29cdef0
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-15 13:46:39 +0900

    mission complete

--- problems/[221215] 1328. Break a Palindrome.js
+++ problems/[221215] 1328. Break a Palindrome.js
@@ -5,5 +5,19 @@
  * @return {string}
  */
 const breakPalindrome = function (palindrome) {
+  if (palindrome.length === 1) return "";
 
+  const result = palindrome.replace(/[^a]/, "a");
+
+  const deduplicate = new Set(result.split(""));
+
+  if (deduplicate.size === 1) {
+    for (let i = palindrome.length - 1; i >= 0; i--) {
+      if (palindrome[i] === "a") {
+        return palindrome.substring(0, i) + "b" + palindrome.substring(i + 1);
+      }
+    }
+  }
+
+  return result;
 };
