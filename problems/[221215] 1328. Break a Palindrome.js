b9b2fee3be00eacbdc9b155357b9f2ae4f3157e0
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-15 11:31:12 +0900

    solve problem

--- problems/[221215] 1328. Break a Palindrome.js
+++ problems/[221215] 1328. Break a Palindrome.js
@@ -4,6 +4,19 @@
  * @param {string} palindrome
  * @return {string}
  */
-const breakPalindrome = function (palindrome) {
+ const breakPalindrome = function(palindrome) {
+  if (palindrome.length === 1) {
+    return "";
+  }
 
+  for (let i = 0; i < palindrome.length; i++) {
+    if (
+      palindrome[i] !== "a"
+      && !(palindrome.length % 2 !== 0 && i === Math.floor(palindrome.length / 2))
+    ) {
+      return palindrome.slice(0, i) + "a" + palindrome.slice(i + 1);
+    }
+  }
+
+  return palindrome.slice(0, palindrome.length - 1) + "b";
 };
