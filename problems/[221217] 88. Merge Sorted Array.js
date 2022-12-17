5543cc87fbcfc1239ef7ef4782f9eeb2bf334fd8
Jimoon <roadzmoon76@gmail.com>
2022-12-18 00:06:15 +0900

    Solve with three pointers

--- problems/[221217] 88. Merge Sorted Array.js
+++ problems/[221217] 88. Merge Sorted Array.js
@@ -8,22 +8,19 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 const merge = function(nums1, m, nums2, n) {
-  const onlyNums1Arr = nums1.slice(0, m);
-  let index1 = 0;
-  let index2 = 0;
+  let index1 = m - 1;
+  let index2 = n - 1;
 
-  for (let i = 0; i < m + n; i++) {
-      const num1 = onlyNums1Arr[index1] === undefined ? Number.MAX_SAFE_INTEGER : onlyNums1Arr[index1];
-      const num2 = nums2[index2] === undefined ? Number.MAX_SAFE_INTEGER : nums2[index2];
+  for (let i = nums1.length - 1; i >= 0; i--) {
+      const num1 = nums1[index1] === undefined ? Number.MIN_SAFE_INTEGER : nums1[index1];
+      const num2 = nums2[index2] === undefined ? Number.MIN_SAFE_INTEGER : nums2[index2];
 
       if (num1 <= num2) {
-          nums1[i] = onlyNums1Arr[index1];
-          index1++;
+          nums1[i] = num2;
+          index2--;
       } else {
-          nums1[i] = nums2[index2];
-          index2++;
+          nums1[i] = num1;
+          index1--;
       }
   }
-
-  return nums1;
 };
