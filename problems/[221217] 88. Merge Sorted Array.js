503bb35e4798f0b7e291b45b3eaab7e4ddd48b1d
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-17 11:52:31 +0900

    update problem

--- problems/[221217] 88. Merge Sorted Array.js
+++ problems/[221217] 88. Merge Sorted Array.js
@@ -1,12 +1,31 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/merge-sorted-array/
  *
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
- */
-const merge = function(nums1, m, nums2, n) {
-    
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = function(nums1, m, nums2, n) {
+  for (let i = 0; i < n; i++) {
+    nums1[i + m] = nums2[i];
+  }
+
+  let left = 0;
+  let right = m;
+
+  while (left !== right) {
+    if (nums1[right] < nums1[left]) {
+      const temp = nums1[left];
+
+      nums1[left] = nums1[right];
+      nums1[right] = temp;
+
+      right += 1;
+      continue;
+    }
+
+    left += 1;
+  }
 };
