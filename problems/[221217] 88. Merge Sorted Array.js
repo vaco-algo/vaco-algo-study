7de5e92e8467aae5b2b2f1203f8e01c187804720
Jimoon <roadzmoon76@gmail.com>
2022-12-17 18:09:47 +0900

    [2022.12.17] 길지문

--- problems/[221217] 88. Merge Sorted Array.js
+++ problems/[221217] 88. Merge Sorted Array.js
@@ -7,6 +7,23 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-const merge = function(nums1, m, nums2, n) {
-    
+const merge = function(nums1, m, nums2, n) {
+  const onlyNums1Arr = nums1.slice(0, m);
+  let index1 = 0;
+  let index2 = 0;
+
+  for (let i = 0; i < m + n; i++) {
+      const num1 = onlyNums1Arr[index1] === undefined ? Number.MAX_SAFE_INTEGER : onlyNums1Arr[index1];
+      const num2 = nums2[index2] === undefined ? Number.MAX_SAFE_INTEGER : nums2[index2];
+
+      if (num1 <= num2) {
+          nums1[i] = onlyNums1Arr[index1];
+          index1++;
+      } else {
+          nums1[i] = nums2[index2];
+          index2++;
+      }
+  }
+
+  return nums1;
 };
