d5f3f99b1c219f2657c662bcf2b51639766f16a2
rktnsinger <rktsinger@gmail.com>
2022-12-20 12:13:34 +0900

    WA : 1971. Find if Path Exists in Graph

--- problems/[221220] 1971. Find if Path Exists in Graph.js
+++ problems/[221220] 1971. Find if Path Exists in Graph.js
@@ -8,5 +8,28 @@
  * @return {boolean}
  */
 const validPath = function(n, edges, source, destination) {
+  const queue = [source];
+  const copiedEdges = [...edges];
+  const enqueue = (vertex) => queue.push(vertex);
+  const dequeue = () => queue.shift();
 
+  const visitedVertexes = Array.from({ length: n }).fill(false);
+  visitedVertexes[source] = true;
+
+  while (queue.length > 0) {
+    const currentVertex = dequeue();
+
+    if (currentVertex === destination) return true;
+
+    const currentEdge = copiedEdges[currentVertex];
+
+    for (let i = 0; i < currentEdge.length; i++) {
+      if (visitedVertexes[currentEdge[i]]) continue;
+
+      enqueue(currentEdge[i]);
+      visitedVertexes[currentEdge[i]] = true;
+    }
+  }
+
+  return false;
 };
