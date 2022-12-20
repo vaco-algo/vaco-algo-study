395fdda7f8f5cd294ea8007625f541ce1a67535c
tpdud406 <tpdud406@naver.com>
2022-12-20 12:06:48 +0900

    [221220] 1971. Find if Path Exists in Graph.js

--- problems/[221220] 1971. Find if Path Exists in Graph.js
+++ problems/[221220] 1971. Find if Path Exists in Graph.js
@@ -7,6 +7,22 @@
  * @param {number} destination
  * @return {boolean}
  */
-const validPath = function(n, edges, source, destination) {
+const validPath = function (n, edges, source, destination) {
+  const copyEdges = [...edges];
+  let isValidPath = true;
 
+  const flatAndCountEdges = copyEdges.flat().reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
+
+  const ArrayToBeRepeated = Object.values(flatAndCountEdges);
+
+  for (let i = 0; i < ArrayToBeRepeated; i++) {
+    if (ArrayToBeRepeated[i] !== 2) {
+      isValidPath = false;
+    }
+  }
+
+  return isValidPath;
 };
