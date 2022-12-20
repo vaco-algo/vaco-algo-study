7691cfb03d575cedd3ec71e277e02e19a94ec19c
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-20 11:49:22 +0900

    solve problem

--- problems/[221220] 1971. Find if Path Exists in Graph.js
+++ problems/[221220] 1971. Find if Path Exists in Graph.js
@@ -8,5 +8,24 @@
  * @return {boolean}
  */
 const validPath = function(n, edges, source, destination) {
+  if (paths > 10) {
+    return false;
+  } 
 
+  if (n === 1) {
+    return true;
+  }
+
+  const relations = edges
+    .filter((edge) => edge.includes(source))
+    .map(([first, last]) => first === source ? last : first)
+
+  if (relations.includes(destination)) {
+    return true;
+  }
+
+  const restEdges = edges
+    .filter((edge) => !edge.includes(source));
+
+  return relations.some((rel) => validPath(n, restEdges, rel, destination, paths + 1));
 };
