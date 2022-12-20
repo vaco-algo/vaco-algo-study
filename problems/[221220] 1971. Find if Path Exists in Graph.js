8361837317a2674b6525d9242b8f46fcba42dbc4
Jimoon <roadzmoon76@gmail.com>
2022-12-20 12:01:23 +0900

    [2022.12.20] 길지문

--- problems/[221220] 1971. Find if Path Exists in Graph.js
+++ problems/[221220] 1971. Find if Path Exists in Graph.js
@@ -8,5 +8,31 @@
  * @return {boolean}
  */
 const validPath = function(n, edges, source, destination) {
+    const vertices = new Map();
+    let result = false;
 
+    edges.forEach(([vertex1, vertex2]) => {
+        if (vertices.has(vertex1)) {
+            vertices.get(vertex1).push(vertex2);
+        } else {
+            vertices.set(vertex1, [vertex2]);
+        }
+
+        if (vertices.has(vertex2)) {
+            vertices.get(vertex2).push(vertex1);
+        } else {
+            vertices.set(vertex2, [vertex1]);
+        }
+    });
+
+    function dfs([vertex1, vertex2]) {
+        if (vertex1 === destination || vertex2 === destination) return result = true;
+
+        if (vertex1 !== source) dfs(vertices.get(vertex1));
+        if (vertex2 !== source) dfs(vertices.get(vertex2));
+    }
+
+    dfs(vertices.get(source));
+
+    return result;
 };
