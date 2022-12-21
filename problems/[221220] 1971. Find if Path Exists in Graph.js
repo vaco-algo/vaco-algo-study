43a79d86d748af59a3bbacb8c89f94885b934f2b
rktnsinger <rktsinger@gmail.com>
2022-12-22 05:54:14 +0900

    refactor: BFS 사용하여 구현 (Accepted)

--- problems/[221220] 1971. Find if Path Exists in Graph.js
+++ problems/[221220] 1971. Find if Path Exists in Graph.js
@@ -6,30 +6,106 @@
  * @param {number} source
  * @param {number} destination
  * @return {boolean}
+ *
  */
-const validPath = function(n, edges, source, destination) {
-  const queue = [source];
-  const copiedEdges = [...edges];
-  const enqueue = (vertex) => queue.push(vertex);
-  const dequeue = () => queue.shift();
 
-  const visitedVertices = Array.from({ length: n }).fill(false);
-  visitedVertices[source] = true;
+class Graph {
+  constructor(verticesNumber) {
+    this.adjacencyMatrix = Array.from({ length: verticesNumber }, () =>
+      Array.from({ length: verticesNumber }, () => 0)
+    );
 
-  while (queue.length > 0) {
-    const currentVertex = dequeue();
+    this.adjacencyList = {};
 
-    if (currentVertex === destination) return true;
-
-    const currentEdge = copiedEdges[currentVertex];
-
-    for (let i = 0; i < currentEdge.length; i++) {
-      if (visitedVertices[currentEdge[i]]) continue;
-
-      enqueue(currentEdge[i]);
-      visitedVertices[currentEdge[i]] = true;
-    }
+    this.lastVertex = verticesNumber - 1;
   }
 
-  return false;
+  addEdgeToMatrix([vertex1, vertex2]) {
+    if (vertex1 > this.lastVertex || vertex2 > this.lastVertex) return;
+
+    this.adjacencyMatrix[vertex1][vertex2] = 1;
+    this.adjacencyMatrix[vertex2][vertex1] = 1;
+  }
+
+  addEdgeToList([vertex1, vertex2]) {
+    if (vertex1 > this.lastVertex || vertex2 > this.lastVertex) return;
+
+    if (!this.adjacencyList[vertex1]) this.adjacencyList[vertex1] = [];
+    if (!this.adjacencyList[vertex2]) this.adjacencyList[vertex2] = [];
+
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+}
+
+class CustomQueue {
+  constructor() {
+    this.storage = [];
+  }
+
+  enqueue(element) {
+    this.storage.unshift(element);
+  }
+
+  dequeue() {
+    return this.storage.pop();
+  }
+
+  isEmpty() {
+    return !this.storage.length;
+  }
+}
+
+const GRAPH_TYPE = "matrix";
+
+const validPath = function (n, edges, source, destination) {
+  const graph = new Graph(n);
+
+  edges.forEach((edge) => {
+    GRAPH_TYPE === "matrix"
+      ? graph.addEdgeToMatrix(edge)
+      : graph.addEdgeToList(edge);
+  });
+
+  const BFS = (graph, from, to) => {
+    const queue = new CustomQueue();
+    const visitedSet = new Set();
+
+    queue.enqueue(from);
+    visitedSet.add(from);
+
+    while (!queue.isEmpty()) {
+      const currentVertex = queue.dequeue();
+
+      if (currentVertex === to) return true;
+
+      const traverseMatrix = () => {
+        graph.adjacencyMatrix[currentVertex].forEach((value, vertex) => {
+          const isVisitedVertex = visitedSet.has(vertex)
+
+          if (isVisitedVertex || value === 0) return;
+
+          queue.enqueue(vertex);
+          visitedSet.add(vertex);
+        });
+      };
+
+      const traverseList = () => {
+        graph.adjacencyList[currentVertex].forEach((adjancyVertex) => {
+          const isVisitedVertex = visitedSet.has(adjancyVertex);
+
+          if (isVisitedVertex) return;
+
+          queue.enqueue(adjancyVertex);
+          visitedSet.add(adjancyVertex);
+        });
+      };
+
+      GRAPH_TYPE === "matrix" ? traverseMatrix() : traverseList();
+    }
+
+    return false;
+  };
+
+  return BFS(graph, source, destination);
 };
