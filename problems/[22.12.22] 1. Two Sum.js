6b46ba084aaab3178e49ff521904ec44b75ae51e
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-22 11:24:27 +0900

    mission complete

--- problems/[22.12.22] 1. Two Sum.js
+++ problems/[22.12.22] 1. Two Sum.js
@@ -4,6 +4,20 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
+  while (left < right) {
+    if (nums[left] + nums[right] === target) {
+      return [left, right];
+    }
+
+    if (left === right - 1) {
+      left++;
+      right = nums.length - 1;
+    } else {
+      right--;
+    }
+  }
 };
