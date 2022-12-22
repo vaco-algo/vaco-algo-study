adbda9e2715bfaca226b9adc970b40a60cfd5765
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-22 13:51:57 +0900

    Update

--- problems/[22.12.22] 1. Two Sum.js
+++ problems/[22.12.22] 1. Two Sum.js
@@ -4,6 +4,8 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// On^2 모든 경우의 수 탐색
 var twoSum = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
@@ -18,6 +20,22 @@ var twoSum = function (nums, target) {
       right = nums.length - 1;
     } else {
       right--;
+    }
+  }
+};
+
+// On object를 해시테이블로 사용하여 탐색 O2N
+var twoSum2 = function (nums, target) {
+  const storedNumberAndIndex = {};
+
+  nums.forEach((number, index) => (storedNumberAndIndex[number] = index));
+
+  for (let i = 0; i < nums.length; i++) {
+    if (
+      storedNumberAndIndex[target - nums[i]] &&
+      storedNumberAndIndex[target - nums[i]] !== i
+    ) {
+      return [storedNumberAndIndex[target - nums[i]], i];
     }
   }
 };
