47c005f57180dc63e3bd95319fa2d1f7f543c406
csy <csl8804@gmail.com>
2022-12-22 12:49:38 +0900

    feat: Two Sum 풀이

--- problems/[22.12.22] 1. Two Sum.js
+++ problems/[22.12.22] 1. Two Sum.js
@@ -4,6 +4,17 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
+  let left = 0;
+  let right = 1;
 
+  while (right < nums.length) {
+    if (nums[left] + nums[right] === target) {
+      return [left, right];
+    } else if (nums[left] + nums[right] < target) {
+      right += 1;
+    } else if (nums[left] + nums[right] > target) {
+      left += 1;
+    }
+  }
 };
