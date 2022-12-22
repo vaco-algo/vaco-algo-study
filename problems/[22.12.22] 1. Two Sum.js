e038223959e35957c43eef08109250e4df1a097c
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-22 11:45:09 +0900

    solve problem

--- problems/[22.12.22] 1. Two Sum.js
+++ problems/[22.12.22] 1. Two Sum.js
@@ -5,5 +5,30 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+  const sortedNums = nums.slice().sort((a, b) => a - b);
+  let left = 0;
+  let right = sortedNums.length - 1;
 
+  while (left !== right) {
+    const currentSum = sortedNums[left] + sortedNums[right];
+
+    if (currentSum === target) {
+      const sumResult = [sortedNums[left], sortedNums[right]];
+      const result = [];
+
+      for (let i = 0; i < nums.length; i++) {
+        if (sumResult.includes(nums[i])) {
+          result.push(i);
+        }
+      }
+
+      return result;
+    }
+
+    if (currentSum > target) {
+      right -= 1;
+    } else {
+      left += 1;
+    }
+  }
 };
