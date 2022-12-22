7bc9dc9924ed7d25a2b9cbad69c94c417b0920e3
tpdud406 <tpdud406@naver.com>
2022-12-22 11:54:30 +0900

    [22.12.22] 1. Two Sum

--- problems/[22.12.22] 1. Two Sum.js
+++ problems/[22.12.22] 1. Two Sum.js
@@ -4,6 +4,29 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+const twoSum = function (nums, target) {
+  const numbers = [...nums];
+  let result;
 
+  for (let i = 0; i < numbers.length; i++) {
+    const NumberToFind = target - numbers[i];
+    result = [i];
+
+    for (let j = 1; j < numbers.length; j++) {
+      if (i === j) {
+        continue;
+      }
+
+      if (numbers[j] === NumberToFind) {
+        result.push(j);
+        break;
+      }
+    }
+
+    if (result.length === 2) {
+      break;
+    }
+  }
+
+  return result;
 };
