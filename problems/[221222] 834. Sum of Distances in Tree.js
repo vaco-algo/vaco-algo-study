0a68f5d820eb6a70f3553112fb3794e216463eef
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-22 09:49:35 +0900

    (auto upload) 834. Sum of Distances in Tree

---
+++ problems/[221222] 834. Sum of Distances in Tree.js
@@ -0,0 +1,10 @@
+/**
+ * leetcode problem link: https://leetcode.com/problems/sum-of-distances-in-tree/
+ *
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+const sumOfDistancesInTree = function(n, edges) {
+    
+};
