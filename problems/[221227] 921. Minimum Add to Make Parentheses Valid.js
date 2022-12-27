ec5ad71b819b8811255c6fecf09d41b6ad0a6b59
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-27 14:08:38 +0900

    Time out

--- problems/[221227] 921. Minimum Add to Make Parentheses Valid.js
+++ problems/[221227] 921. Minimum Add to Make Parentheses Valid.js
@@ -1,9 +1,23 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
  *
- * @param {string} s
- * @return {number}
- */
-const minAddToMakeValid = function(s) {
-    
+ * @param {string} s
+ * @return {number}
+ */
+const minAddToMakeValid = function (s) {
+  if (s.length === 1) return 1;
+
+  const queue = [s[0]];
+
+  for (let i = 1; i < s.length; i++) {
+    if (queue[queue.length - 1] + s[i] === "()") {
+      queue.pop();
+
+      continue;
+    }
+
+    queue.push(s[i]);
+  }
+
+  return queue.length;
 };
