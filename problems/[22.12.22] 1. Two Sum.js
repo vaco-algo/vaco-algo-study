0ddb2a7af7bc18cf932572dfcf3335e597c27e49
rktnsinger <rktsinger@gmail.com>
2022-12-27 10:52:14 +0900

    Passed: 1. Two Sum

--- problems/[22.12.22] 1. Two Sum.js
+++ problems/[22.12.22] 1. Two Sum.js
@@ -4,6 +4,27 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
 
+var twoSum = function(nums, target) {
+  const hashMap = {};
+  const array = [...nums];
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    hashMap[array[i]] = i;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+
+    if (i === hashMap[complement]) continue;
+
+    if (hashMap[complement] !== undefined) {
+      result.push(i, hashMap[complement]);
+
+      break;
+    }
+  }
+
+  return result;
 };
