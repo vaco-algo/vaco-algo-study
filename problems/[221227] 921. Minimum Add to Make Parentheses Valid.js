52bbb1850dca7eca32605ab05e7959c43f639e8f
rktnsinger <rktsinger@gmail.com>
2022-12-27 12:21:39 +0900

    PASSED: 921. Minimum Add to Make Parentheses Valid

--- problems/[221227] 921. Minimum Add to Make Parentheses Valid.js
+++ problems/[221227] 921. Minimum Add to Make Parentheses Valid.js
@@ -1,9 +1,27 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
  *
- * @param {string} s
- * @return {number}
+ * @param {string} s
+ * @return {number}
  */
-const minAddToMakeValid = function(s) {
-    
+const minAddToMakeValid = function(s) {
+  const parenthesesString = s;
+  const stack = [];
+  let unbalancedCount = 0;
+
+  for (const paren of parenthesesString) {
+    if (!stack.length) {
+      paren === ")"
+        ? unbalancedCount += 1
+        : stack.push(s[i]);
+
+      continue;
+    }
+
+    paren === "("
+      ? stack.push(paren)
+      : stack.pop();
+  }
+
+  return stack.length + unbalancedCount;
 };
