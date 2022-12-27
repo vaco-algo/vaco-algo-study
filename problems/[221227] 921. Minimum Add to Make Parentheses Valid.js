763cf09b3bbe44bbd9e95404bee24e167565a26d
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2022-12-27 14:10:38 +0900

    Chore queue -> stack 이름 변경

--- problems/[221227] 921. Minimum Add to Make Parentheses Valid.js
+++ problems/[221227] 921. Minimum Add to Make Parentheses Valid.js
@@ -7,17 +7,17 @@
 const minAddToMakeValid = function (s) {
   if (s.length === 1) return 1;
 
-  const queue = [s[0]];
+  const stack = [s[0]];
 
   for (let i = 1; i < s.length; i++) {
-    if (queue[queue.length - 1] + s[i] === "()") {
-      queue.pop();
+    if (stack[stack.length - 1] + s[i] === "()") {
+      stack.pop();
 
       continue;
     }
 
-    queue.push(s[i]);
+    stack.push(s[i]);
   }
 
-  return queue.length;
+  return stack.length;
 };
