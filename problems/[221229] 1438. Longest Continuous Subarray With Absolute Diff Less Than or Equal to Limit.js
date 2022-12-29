4f885f2c88a88608f5bdc7baa34cb0559a8e4cdd
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-12-29 12:05:32 +0900

    upload problem 2

--- problems/[221229] 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.js
+++ problems/[221229] 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.js
@@ -6,5 +6,23 @@
  * @return {number}
  */
 const longestSubarray = function(nums, limit) {
-    
+  let result = 1;
+  let numsQueue = [nums[0]];
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < numsQueue.length; j++) {
+      if (
+        numsQueue[j] > nums[i] + limit
+        || numsQueue[j] < nums[i] - limit
+      ) {
+        numsQueue = numsQueue.slice(j + 1);
+        j = -1;
+      }
+    }
+
+    numsQueue.push(nums[i]);
+    result = Math.max(result, numsQueue.length);
+  }
+
+  return result;
 };
