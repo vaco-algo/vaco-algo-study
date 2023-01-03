050dc54bcdb4b99fd52b87477b2921bf12f2d06f
rktnsinger <rktsinger@gmail.com>
2023-01-03 12:08:20 +0900

    WA: 141-Linked List Cycle

--- problems/[230103] 141. Linked List Cycle.js
+++ problems/[230103] 141. Linked List Cycle.js
@@ -8,9 +8,21 @@
  * }
  */
 /**
- * @param {ListNode} head
- * @return {boolean}
- */
-const hasCycle = function(head) {
-    
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = function(head) {
+  if (!head) return false;
+
+  const visitedSet = new Set();
+  let currentNode = head;
+
+  while (true) {
+    if (!currentNode.next) return false;
+
+    if (visitedSet.has(currentNode.val)) return true;
+
+    visitedSet.add(currentNode.val);
+    currentNode = currentNode.next;
+  }
 };
