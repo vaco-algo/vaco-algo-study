877111d77abae0804f7ea8ff720b67b9c896c7c7
eunhye210 <tkrhd0210@gmail.com>
2023-01-05 11:50:42 +0900

    230105 algo

--- problems/[230105] 82. Remove Duplicates from Sorted List II.js
+++ problems/[230105] 82. Remove Duplicates from Sorted List II.js
@@ -12,5 +12,35 @@
  * @return {ListNode}
  */
 const deleteDuplicates = function(head) {
-    
+  if (!head) return head;
+
+  let removedLinkedList = null;
+
+  let prev = head;
+  let curr = head.next;
+  const duplicates = [];
+  let currentNode = null;
+
+  while (prev) {
+    if (prev.val !== curr?.val && duplicates.indexOf(prev.val) === -1) {
+      const newNode = new ListNode(prev.val);
+
+      if (!removedLinkedList) {
+        removedLinkedList = newNode;
+        currentNode = removedLinkedList;
+      } else {
+        currentNode.next = newNode;
+        currentNode = currentNode.next;
+      }
+    }
+
+    if (prev.val === curr?.val) {
+      duplicates.push(prev.val);
+    }
+
+    prev = curr;
+    curr = curr?.next;
+  }
+
+  return removedLinkedList;
 };
