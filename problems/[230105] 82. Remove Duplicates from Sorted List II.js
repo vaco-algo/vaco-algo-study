18dc55b9b97a70915bf8c00cc5e32607b49c7711
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-01-05 12:17:56 +0900

    solve

--- problems/[230105] 82. Remove Duplicates from Sorted List II.js
+++ problems/[230105] 82. Remove Duplicates from Sorted List II.js
@@ -12,5 +12,37 @@
  * @return {ListNode}
  */
 const deleteDuplicates = function(head) {
-    
+  const nodeHistory = new Map();
+  let currentNode = head;
+
+  while (currentNode?.val || currentNode?.val === 0) {
+    nodeHistory.set(
+      currentNode.val,
+      nodeHistory.has(currentNode.val)
+        ? nodeHistory.get(currentNode.val) + 1
+        : 1
+    );
+    currentNode = currentNode.next;
+  }
+
+  currentNode = head;
+  let nonDuplicatedHead = null;
+  let currentNonDuplicated = null;
+
+  while (currentNode?.val || currentNode?.val === 0) {
+    if (nodeHistory.get(currentNode.val) === 1) {
+      if (!nonDuplicatedHead) {
+        nonDuplicatedHead = currentNode;
+        currentNonDuplicated = nonDuplicatedHead;
+      } else {
+        currentNonDuplicated.next = currentNode;
+        currentNonDuplicated = currentNonDuplicated.next;
+      }
+    }
+
+    currentNode = currentNode.next;
+  }
+
+  currentNonDuplicated?.next && (currentNonDuplicated.next = null);
+  return nonDuplicatedHead;
 };
