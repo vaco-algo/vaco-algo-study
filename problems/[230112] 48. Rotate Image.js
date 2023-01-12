1eede3a39cdf43bd34512778c45d73ffd0e83535
rktnsinger <rktsinger@gmail.com>
2023-01-12 12:18:40 +0900

    refactor: 임시배열을 Array.from 메소드 통해 간단히 구현

--- problems/[230112] 48. Rotate Image.js
+++ problems/[230112] 48. Rotate Image.js
@@ -5,21 +5,16 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 const rotate = function (matrix) {
-  const tempMatrix = [];
-  const length = matrix.length;
+  const tempMatrix = Array.from({ length: matrix.length }, () => []);
 
-  for (let i = 0; i < matrix.length; i++) {
-    tempMatrix.push([]);
-  }
-
-  for (let i = 0; i < length; i++) {
-    for (let j = 0; j < length; j++) {
+  for (let i = 0; i < tempMatrix.length; i++) {
+    for (let j = 0; j < tempMatrix.length; j++) {
       tempMatrix[i].push(matrix[i].shift());
     }
   }
 
-  for (let i = length - 1; i >= 0; i--) {
-    for (let j = length - 1; j >= 0; j--) {
+  for (let i = tempMatrix.length - 1; i >= 0; i--) {
+    for (let j = tempMatrix.length - 1; j >= 0; j--) {
       matrix[j].push(tempMatrix[i].pop());
     }
   }
