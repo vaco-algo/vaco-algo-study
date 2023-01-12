1c455f1855743696ba0dfe135cc4e880fe97d4a5
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-01-12 11:32:01 +0900

    solve

--- problems/[230112] 48. Rotate Image.js
+++ problems/[230112] 48. Rotate Image.js
@@ -4,6 +4,16 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-const rotate = function(matrix) {
-    
+const rotate = function (matrix) {
+  matrix
+    .reverse()
+    .forEach((row, rowIdx, arr) => {
+      for (let i = rowIdx; i < row.length; i++) {
+        const temp = arr[i][rowIdx];
+        arr[i][rowIdx] = row[i];
+        row[i] = temp;
+      }
+    });
+
+  return;
 };
