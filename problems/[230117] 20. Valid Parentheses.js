d2a1f16f5074f6d6bb3e972434bae29794b60989
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-17 11:40:29 +0900

    mission complete

--- problems/[230117] 20. Valid Parentheses.js
+++ problems/[230117] 20. Valid Parentheses.js
@@ -3,7 +3,29 @@
  *
  * @param {string} s
  * @return {boolean}
- */
-const isValid = function(s) {
-   
+ */
+const isValid = function (s) {
+  if (s.length === 1 || new Set(s).size === 1) return false;
+
+  const leftParentheses = [];
+
+  for (let char of s) {
+    if (char === "(" || char === "{" || char === "[") {
+      leftParentheses.push(char);
+    }
+
+    if (char === ")") {
+      if (leftParentheses.pop() !== "(") return false;
+    }
+
+    if (char === "}") {
+      if (leftParentheses.pop() !== "{") return false;
+    }
+
+    if (char === "]") {
+      if (leftParentheses.pop() !== "[") return false;
+    }
+  }
+
+  return leftParentheses.length ? false : true;
 };
