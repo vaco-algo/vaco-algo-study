51062e28d2b7294e8138aa61394e0945dd78ed72
tpdud406 <tpdud406@naver.com>
2023-01-17 12:09:57 +0900

    20230117 isValid

--- problems/[230117] 20. Valid Parentheses.js
+++ problems/[230117] 20. Valid Parentheses.js
@@ -3,7 +3,24 @@
  *
  * @param {string} s
  * @return {boolean}
- */
-const isValid = function(s) {
-   
+ */
+const isValid = function (s) {
+  const splitParams = s.split("");
+  const pair = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (let i = 0; i < splitParams.length; i += 2) {
+    if (pair[splitParams[i]] === splitParams[i + 1]) {
+      //   splitParams.splice(0, 2);
+
+      continue;
+    }
+
+    return false;
+  }
+
+  return true;
 };
