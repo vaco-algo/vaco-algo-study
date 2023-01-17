9fdfebb1bbe9fea8c102c62c5f8c39610eefce91
eunhye210 <tkrhd0210@gmail.com>
2023-01-17 11:36:03 +0900

    230117 algo

--- problems/[230117] 20. Valid Parentheses.js
+++ problems/[230117] 20. Valid Parentheses.js
@@ -5,5 +5,22 @@
  * @return {boolean}
  */
 const isValid = function(s) {
-   
+  if (s.length <= 1) return !s.length;
+
+  const stack = [];
+  const pairBrackets = {
+    ")" : "(",
+    "}" : "{",
+    "]" : "[",
+  };
+
+  s.split("").forEach((item) => {
+    if (pairBrackets[item] && pairBrackets[item] === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(item);
+    }
+  });
+
+  return stack.length === 0;
 };
