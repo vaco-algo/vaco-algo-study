985bed915faf0511aa255c8e2579fb98f4ef4a09
csy <csl8804@gmail.com>
2023-01-17 13:29:47 +0900

    feat: Valid Parentheses 풀이

--- problems/[230117] 20. Valid Parentheses.js
+++ problems/[230117] 20. Valid Parentheses.js
@@ -3,7 +3,33 @@
  *
  * @param {string} s
  * @return {boolean}
- */
-const isValid = function(s) {
-   
+ */
+const isValid = function (s) {
+  /**
+   * @param {string} s
+   * @return {boolean}
+   */
+  var isValid = function (s) {
+    const stack = [];
+
+    for (let char of s) {
+      if (char === "(" | "{" | "[") {
+        stack.push(char);
+      }
+
+      if (char === ")") {
+        if (stack.pop() !== "(") return false;
+      }
+
+      if (char === "}") {
+        if (stack.pop() !== "{") return false;
+      }
+
+      if (char === "]") {
+        if (stack.pop() !== "[") return false;
+      }
+    }
+
+    return stack.length === 0;
+  };
 };
