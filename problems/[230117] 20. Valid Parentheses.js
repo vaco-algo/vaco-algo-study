b091349a334894094c2d9ced46ebbdeb6643fab9
rktnsinger <rktsinger@gmail.com>
2023-01-17 11:44:08 +0900

    PASSED: 20. Valid Parentheses

--- problems/[230117] 20. Valid Parentheses.js
+++ problems/[230117] 20. Valid Parentheses.js
@@ -3,7 +3,31 @@
  *
  * @param {string} s
  * @return {boolean}
- */
-const isValid = function(s) {
-   
+ */
+const isValid = function (s) {
+  const stack = [];
+
+  for (const paren of s) {
+    if (paren === "(" || paren === "{" || paren === "[") {
+      stack.push(paren);
+
+      continue;
+    }
+
+    const peek = stack[stack.length - 1];
+
+    if (!peek) return false;
+
+    if (
+      (peek === "(" && paren === ")") ||
+      (peek === "{" && paren === "}") ||
+      (peek === "[" && paren === "]")
+    ) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return !stack.length ? true : false;
 };
