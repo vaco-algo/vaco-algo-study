a4eaf2b4ed8aee540eaf43de50821ee4bf520202
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-01-20 15:20:43 +0900

    solve problem

--- problems/[230119] 21. Merge Two Sorted Lists.js
+++ problems/[230119] 21. Merge Two Sorted Lists.js
@@ -13,5 +13,25 @@
  * @return {ListNode}
  */
 const mergeTwoLists = function(list1, list2) {
-  
+  const resultHead = new ListNode();
+  let currentResultNode = resultHead;
+  let currentList1Node = list1;
+  let currentList2Node = list2;
+
+  while (!isNaN(currentList1Node?.val) || !isNaN(currentList2Node?.val)) {
+    let min = null;
+
+    if ((currentList1Node?.val ?? 101) < (currentList2Node?.val ?? 101)) {
+      min = currentList1Node.val;
+      currentList1Node = currentList1Node.next;
+    } else {
+      min = currentList2Node.val;
+      currentList2Node = currentList2Node.next;
+    }
+
+    currentResultNode.next = new ListNode(min);
+    currentResultNode = currentResultNode.next;
+  }
+
+  return resultHead.next;
 };
