883d680a44b094d82b0cf13c313cb1229a3438a2
Areum Han <hanryu1109@gmail.com>
2023-01-21 21:00:17 +0900

    add: solve problem

--- problems/[230119] 21. Merge Two Sorted Lists.js
+++ problems/[230119] 21. Merge Two Sorted Lists.js
@@ -12,6 +12,43 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-const mergeTwoLists = function(list1, list2) {
-  
+const mergeTwoLists = function (list1, list2) {
+  if (!list1 && !list2) {
+    return list1;
+  }
+
+  if (!list1 && list2) {
+    return list2;
+  }
+
+  if (list1 && !list2) {
+    return list1;
+  }
+
+  let firstNodeList = list1;
+  let secondNodeList = list2;
+
+  const array = [];
+
+  while (true) {
+    if (firstNodeList !== null) {
+      array.push(firstNodeList);
+      firstNodeList = firstNodeList.next;
+    } else if (secondNodeList !== null) {
+      array.push(secondNodeList);
+      secondNodeList = secondNodeList.next;
+    } else {
+      break;
+    }
+  }
+
+  array.sort((a, b) => {
+    return a.val - b.val;
+  });
+
+  for (let i = 0; i < array.length - 1; i++) {
+    array[i].next = array[i + 1];
+  }
+
+  return array[0];
 };
