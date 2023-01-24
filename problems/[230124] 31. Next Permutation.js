2e4c56db18b2539a4155373ed1d53d82dc752d37
Areum Han <hanryu1109@gmail.com>
2023-01-24 17:41:54 +0900

    not solve: next permutation

--- problems/[230124] 31. Next Permutation.js
+++ problems/[230124] 31. Next Permutation.js
@@ -4,6 +4,24 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const nextPermutation = function(nums) {
-    
+const nextPermutation = function (nums) {
+  const result = [];
+
+  const dfs = (i, nums) => {
+    if (i === nums.length) {
+      return result.push([...nums]);
+    }
+
+    for (let j = i; j < nums.length; j++) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+
+      dfs(i + 1, nums);
+
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  };
+
+  dfs(0, nums);
+
+  return result[1];
 };
