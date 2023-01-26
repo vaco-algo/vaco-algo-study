909a9d4957fd0059156bf24b1a552d2ddfbf1f83
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-01-26 11:52:31 +0900

    fail

--- problems/[230126] 32. Longest Valid Parentheses.js
+++ problems/[230126] 32. Longest Valid Parentheses.js
@@ -4,6 +4,25 @@
  * @param {string} s
  * @return {number}
  */
-const longestValidParentheses = function(s) {
-    
+const longestValidParentheses = function (s) {
+  const stack = [];
+  let count = 0;
+  let maxCount = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(s[i]);
+    } else {
+      const pop = stack.pop();
+
+      if (pop === "(") {
+        count += 2;
+      } else {
+        maxCount = count > maxCount ? count : maxCount;
+        count = 0;
+      }
+    }
+  }
+
+  return count > maxCount ? count : maxCount;
 };
