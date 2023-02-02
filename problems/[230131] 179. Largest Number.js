aa82ec56761d3a21ee2e1d81142ca7a9bbe4f1c1
Areum Han <hanryu1109@gmail.com>
2023-02-03 02:39:22 +0900

    Solve: Largest Number

--- problems/[230131] 179. Largest Number.js
+++ problems/[230131] 179. Largest Number.js
@@ -4,6 +4,12 @@
  * @param {number[]} nums
  * @return {string}
  */
-const largestNumber = function(nums) {
-    
+const largestNumber = function (nums) {
+  if (Math.max(...nums) === 0) {
+    return "0";
+  }
+
+  const array = nums.sort((a, b) => Number(`${b}${a}`) - Number(`${a}${b}`));
+
+  return array.join("");
 };
