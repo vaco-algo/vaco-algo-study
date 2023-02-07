585d0f5d7fb8a2f8be323edac1e629dbdc9668ea
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-02-07 11:38:18 +0900

    mission complete

--- problems/[230207] 7. Reverse Integer.js
+++ problems/[230207] 7. Reverse Integer.js
@@ -4,6 +4,17 @@
  * @param {number} x
  * @return {number}
  */
-const reverse = function(x) {
+const reverse = function (x) {
+  const range = 2147483648;
+  let isNegative = x < 0 ? -1 : 1;
 
+  const reverseXToString = String(x * isNegative)
+    .split("")
+    .reverse()
+    .join("");
+  const reverseX = Number(reverseXToString) * isNegative;
+
+  if ((reverseX && reverseX > range) || (reverseX <= 0 && reverseX < range * isNegative)) return 0;
+
+  return reverseX;
 };
