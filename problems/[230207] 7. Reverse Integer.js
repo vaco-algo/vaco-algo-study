f4af2323961f49bf439d648d4a688717ec178724
taeuk <dcjimin@gmail.com>
2023-02-07 12:30:54 +0900

    solve 230207

--- problems/[230207] 7. Reverse Integer.js
+++ problems/[230207] 7. Reverse Integer.js
@@ -4,6 +4,18 @@
  * @param {number} x
  * @return {number}
  */
-const reverse = function(x) {
+const reverse = function (x) {
+  let currentX = x;
+  let digit = Math.abs(x).toString().length - 1;
+  let result = 0;
 
+  while (digit >= 0) {
+    result += (currentX % 10) * Math.pow(10, digit);
+    currentX = parseInt(currentX / 10);
+    digit -= 1;
+  }
+
+  return Math.pow(-2, 31) <= result && result <= Math.pow(2, 31) - 1
+    ? result
+    : 0;
 };
