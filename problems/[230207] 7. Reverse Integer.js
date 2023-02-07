da32536c3e75ba38b81e2c2f1d4f5bba0b5f5c28
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-02-07 11:43:06 +0900

    solve problem

--- problems/[230207] 7. Reverse Integer.js
+++ problems/[230207] 7. Reverse Integer.js
@@ -5,5 +5,21 @@
  * @return {number}
  */
 const reverse = function(x) {
+  let stringX = Math.abs(x).toString();
+  let reversedString = "";
 
+  for (let i = 0; i < stringX.length; i++) {
+    reversedString += stringX[stringX.length - i - 1];
+  }
+
+  if (
+    reversedString.length > 10
+    || (reversedString.length === 10 && reversedString > "2147483647")
+    ) {
+    return 0;
+  }
+
+  return x < 0
+    ? reversedString * -1
+    : reversedString * 1;
 };
