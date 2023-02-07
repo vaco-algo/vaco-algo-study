cea8b35a101f773d4e48ee6651543068fad359c6
rktnsinger <rktsinger@gmail.com>
2023-02-07 12:16:29 +0900

    PASSED: 7. Reverse Integer

--- problems/[230207] 7. Reverse Integer.js
+++ problems/[230207] 7. Reverse Integer.js
@@ -4,6 +4,28 @@
  * @param {number} x
  * @return {number}
  */
-const reverse = function(x) {
 
+const reverseInteger = (integer) => {
+  const integerString = integer + "";
+  let reversedString = "";
+
+  for (let i = integerString.length - 1; i >= 0; i--) {
+    if (integerString[i] === "-") {
+      reversedString = "-" + reversedString;
+
+      break;
+    }
+
+    reversedString += integerString[i];
+  }
+
+  return Number(reversedString);
+};
+
+const reverse = function (x) {
+  const reversedX = reverseInteger(x);
+
+  if (reversedX < Math.pow(-2, 31) || reversedX > Math.pow(2, 31) - 1) return 0;
+
+  return reversedX;
 };
