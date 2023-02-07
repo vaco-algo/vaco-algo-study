208d9fd83855ebbc412c05aecb26d67e4ede9ef6
comt.mix@gmail.com <comt.mix@gmail.com>
2023-02-07 11:51:38 +0900

    [230207] Reverse Integer

--- problems/[230207] 7. Reverse Integer.js
+++ problems/[230207] 7. Reverse Integer.js
@@ -4,6 +4,27 @@
  * @param {number} x
  * @return {number}
  */
-const reverse = function(x) {
+const reverse = function (x) {
+  let str = x.toString().split("");
+  let result;
 
+  const min = -Math.pow(2, 31);
+  const max = Math.pow(2, 31) - 1;
+
+  if (x === 0) return 0;
+  if (x <= min || x >= max) return 0;
+
+  if (x < 0) {
+    result = str.reverse().join("");
+    result = result.replace(/[^0-9]/g, "");
+    if (Number("-" + result) <= min) return 0;
+
+    return Number("-" + result);
+  }
+  if (x > 0) {
+    result = str.reverse().join("");
+    if (result >= max) return 0;
+
+    return Number(result);
+  }
 };
