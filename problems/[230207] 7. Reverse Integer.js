f3702a1f85dd93234543705fde39fea20d292f13
ssong-yi <sychoi@sychoiui-MacBookPro.local>
2023-02-07 17:04:07 +0900

    feat: Reverse Integer

--- problems/[230207] 7. Reverse Integer.js
+++ problems/[230207] 7. Reverse Integer.js
@@ -4,6 +4,13 @@
  * @param {number} x
  * @return {number}
  */
-const reverse = function(x) {
+const reverse = function (x) {
+    const string = Math.abs(x).toString();
+    let reversedNumber = Number(string.split("").reverse().join(""));
 
+    if (x < 0) reversedNumber = reversedNumber * -1;
+
+    if (reversedNumber > Math.pow(2, 31) - 1 || reversedNumber < Math.pow(-2, 31)) return 0;
+
+    return reversedNumber;
 };
