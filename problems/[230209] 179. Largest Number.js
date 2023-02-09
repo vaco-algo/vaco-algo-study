462ff319ae57733757beeef064e78f848bcbd138
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-02-09 14:47:45 +0900

    fail

--- problems/[230209] 179. Largest Number.js
+++ problems/[230209] 179. Largest Number.js
@@ -4,6 +4,66 @@
  * @param {number[]} nums
  * @return {string}
  */
-const largestNumber = function(nums) {
+function permutation(arr, originalLength) {
+  if (arr.length === 1) return [arr];
+  const result = [];
 
+  arr.forEach((fixed, index) => {
+    const rest = [...arr.slice(0, index), ...arr.slice(index + 1)];
+    const combinations = permutation(rest, originalLength);
+    const attach = combinations.map((combination) => {
+      if (arr.length !== originalLength) return [fixed, ...combination];
+
+      return [fixed, ...combination].join("");
+    });
+
+    result.push(...attach);
+  });
+
+  return result;
+}
+
+var largestNumber = function (nums) {
+  if (nums.length === 1 || (new Set(nums).size === 1 && nums[0] === 0))
+    return nums[0] + "";
+
+  let result = "";
+  const keep = [];
+
+  const sortNums = [...nums].sort().reverse();
+
+  sortNums.forEach((num, index) => {
+    const currentStringNum = num + "";
+
+    if (keep.length) {
+      if (keep[0][0] !== currentStringNum[0]) {
+        const test = permutation(keep, keep.length);
+        test.sort();
+        result += test.pop();
+
+        keep.length = 0;
+      } else if (index === sortNums.length - 1) {
+        keep.push(currentStringNum);
+        const test = permutation(keep, keep.length);
+        test.sort();
+
+        return (result += test.pop());
+      } else {
+        return keep.push(currentStringNum);
+      }
+    }
+
+    if (
+      index !== sortNums.length - 1 &&
+      currentStringNum.length > 1 &&
+      currentStringNum.charCodeAt(0) ===
+        sortNums[index + 1].toString().charCodeAt(0)
+    ) {
+      return keep.push(currentStringNum);
+    }
+
+    return (result += currentStringNum);
+  });
+
+  return result;
 };
