1fc6b242fe11d09ab7be756450b37c858341aae2
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-02-10 20:15:22 +0900

    solve problem:

--- problems/[230209] 179. Largest Number.js
+++ problems/[230209] 179. Largest Number.js
@@ -5,5 +5,9 @@
  * @return {string}
  */
 const largestNumber = function(nums) {
+  const result = nums.sort((a, b) => `${b}${a}` - `${a}${b}`);
 
+  while(result[0] === 0 && result.length > 1) result.splice(0, 1);
+
+  return result.join("");
 };
