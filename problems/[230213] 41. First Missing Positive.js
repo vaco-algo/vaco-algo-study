91123f3fe1a5866d83e2ef96f42808c9fbba7ea2
ssong-yi <syuvc8804@gmail.com>
2023-02-14 12:44:19 +0900

    refactor: sort함수 삭제

--- problems/[230213] 41. First Missing Positive.js
+++ problems/[230213] 41. First Missing Positive.js
@@ -5,10 +5,9 @@
  * @return {number}
  */
 const firstMissingPositive = function (nums) {
-    const sortedNums = nums.sort((a, b) => a - b)
     const sequence = Array.from({ length: nums.length + 1 }, (_, i) => i + 1);
 
     for (let i = 0; i < sequence.length; i++) {
-        if (!sortedNums.includes(sequence[i])) return sequence[i];
+        if (!nums.includes(sequence[i])) return sequence[i];
     }
 };
