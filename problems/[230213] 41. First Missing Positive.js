73b75a64f2f01c65753eb19b72d846ec2c07a2eb
ssong-yi <syuvc8804@gmail.com>
2023-02-14 12:38:49 +0900

    feat: firstMissingPositive

--- problems/[230213] 41. First Missing Positive.js
+++ problems/[230213] 41. First Missing Positive.js
@@ -1,9 +1,14 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/first-missing-positive
  *
- * @param {number[]} nums
- * @return {number}
- */
-const firstMissingPositive = function(nums) {
-    
+ * @param {number[]} nums
+ * @return {number}
+ */
+const firstMissingPositive = function (nums) {
+    const sortedNums = nums.sort((a, b) => a - b)
+    const sequence = Array.from({ length: nums.length + 1 }, (_, i) => i + 1);
+
+    for (let i = 0; i < sequence.length; i++) {
+        if (!sortedNums.includes(sequence[i])) return sequence[i];
+    }
 };
