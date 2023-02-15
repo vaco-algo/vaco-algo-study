fbcf72dbbfc6ae57bcad2a91d99877b0acf734ab
ssong-yi <syuvc8804@gmail.com>
2023-02-15 13:03:05 +0900

    refactor: sequence 배열 로직 삭제

--- problems/[230213] 41. First Missing Positive.js
+++ problems/[230213] 41. First Missing Positive.js
@@ -5,9 +5,7 @@
  * @return {number}
  */
 const firstMissingPositive = function (nums) {
-    const sequence = Array.from({ length: nums.length + 1 }, (_, i) => i + 1);
-
-    for (let i = 0; i < sequence.length; i++) {
-        if (!nums.includes(sequence[i])) return sequence[i];
+    for (let i = 1; i < nums.length + 2; i++) {
+        if (!nums.includes(i)) return i;
     }
 };
