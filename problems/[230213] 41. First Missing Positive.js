c521a268f9dce1d789a65714a9dd057f52bcd67b
eunhye210 <tkrhd0210@gmail.com>
2023-02-15 21:28:05 +0900

    230215 algo

--- problems/[230213] 41. First Missing Positive.js
+++ problems/[230213] 41. First Missing Positive.js
@@ -1,9 +1,23 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/first-missing-positive
  *
- * @param {number[]} nums
- * @return {number}
- */
-const firstMissingPositive = function(nums) {
-    
+ * @param {number[]} nums
+ * @return {number}
+ */
+const firstMissingPositive = function (nums) {
+  let result = 1;
+  const sortedNums = nums.sort((a, b) => a - b);
+  const sorteDeduplicatedNums = [...new Set(sortedNums)];
+
+  for (const num of sorteDeduplicatedNums) {
+    if (num <= 0) continue;
+    if (result === num) {
+      result++;
+      continue;
+    }
+
+    break;
+  }
+
+  return result;
 };
