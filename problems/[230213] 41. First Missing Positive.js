73b9ce5c9d6c076784f02a7abf6728e85ad390ae
tpdud406 <tpdud406@naver.com>
2023-02-15 21:51:22 +0900

    [230213] 41. First Missing Positive

--- problems/[230213] 41. First Missing Positive.js
+++ problems/[230213] 41. First Missing Positive.js
@@ -1,9 +1,52 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/first-missing-positive
  *
- * @param {number[]} nums
- * @return {number}
- */
-const firstMissingPositive = function(nums) {
-    
+ * @param {number[]} nums
+ * @return {number}
+ */
+const firstMissingPositive = function (nums) {
+  const Deduplication = new Set(nums);
+  const sortedNums = [...Deduplication].sort((a, b) => a - b);
+  let count = 0;
+  let index = 0;
+
+  if (sortedNums.length === 1) {
+    if (sortedNums[0] > 1 || sortedNums[0] < 0) {
+      return 1;
+    } else {
+      return sortedNums[0] + 1;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (sortedNums[i] <= 0) {
+      count++;
+
+      if (count === nums.length) {
+        return 1;
+      }
+
+      if (sortedNums.length === 1 || count === sortedNums.length) {
+        return 1;
+      }
+
+      continue;
+    } else {
+      index++;
+
+      if (index === 1 && sortedNums[i] !== 1) {
+        return 1;
+      }
+
+      if (index === sortedNums[i]) {
+        if (index === sortedNums[sortedNums.length - 1]) {
+          return sortedNums[sortedNums.length - 1] + 1;
+        }
+
+        continue;
+      } else {
+        return index;
+      }
+    }
+  }
 };
