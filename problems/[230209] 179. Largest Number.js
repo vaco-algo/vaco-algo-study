2bfe7a761df83558b9431018912b225f0b8f8832
taeuk <dcjimin@gmail.com>
2023-02-16 22:56:21 +0900

    230216

--- problems/[230209] 179. Largest Number.js
+++ problems/[230209] 179. Largest Number.js
@@ -4,6 +4,17 @@
  * @param {number[]} nums
  * @return {string}
  */
-const largestNumber = function(nums) {
+const largestNumber = function (nums) {
+  const result = nums
+    .sort((a, b) => {
+      if (a === b) return 0;
 
+      const stringA = a.toString();
+      const stringB = b.toString();
+
+      return Number(stringB + stringA) - Number(stringA + stringB);
+    })
+    .join("");
+
+  return Number(result) ? result : "0";
 };
