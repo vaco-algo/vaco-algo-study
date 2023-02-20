74cb2354c044566a817c858628b8d4cf2a294db0
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-02-20 22:21:17 +0900

    mission complete

--- problems/[230220] 73. Set Matrix Zeroes.js
+++ problems/[230220] 73. Set Matrix Zeroes.js
@@ -1,9 +1,29 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/set-matrix-zeroes
  *
- * @param {number[][]} matrix
- * @return {void} Do not return anything, modify matrix in-place instead.
- */
-const setZeroes = function(matrix) {
-    
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const setZeroes = function (matrix) {
+  const columns = {};
+
+  for (let row of matrix) {
+    if (!row.includes(0)) {
+      continue;
+    }
+
+    for (let i = 0; i < row.length; i++) {
+      if (row[i] === 0) {
+        columns[i] = 0;
+      }
+
+      row[i] = 0;
+    }
+  }
+
+  for (let col of Object.keys(columns)) {
+    for (let row of matrix) {
+      row[col] = 0;
+    }
+  }
 };
