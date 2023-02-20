dbe7ada6949daa6a9908075591685edf5d049cda
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-02-20 11:02:24 +0900

    solve

--- problems/[230220] 73. Set Matrix Zeroes.js
+++ problems/[230220] 73. Set Matrix Zeroes.js
@@ -4,6 +4,29 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-const setZeroes = function(matrix) {
-    
+const setZeroes = function(matrix) {
+  const zeroCols = new Set();
+  const zeroRows = new Set();
+
+  for (const [colIdx, row] of matrix.entries()) {
+    for (const [rowIdx, cell] of row.entries()) {
+      if (cell === 0) {
+        zeroCols.add(colIdx);
+        zeroRows.add(rowIdx);
+      }
+    }
+  }
+
+  for (const [colIdx, row] of matrix.entries()) {
+    if (zeroCols.has(colIdx)) {
+      matrix[colIdx] = Array.from({ length: row.length }, () => 0);
+      continue;
+    }
+
+    for (const [rowIdx, cell] of row.entries()) {
+      if (zeroRows.has(rowIdx)) {
+        row[rowIdx] = 0;
+      }
+    }
+  }
 };
