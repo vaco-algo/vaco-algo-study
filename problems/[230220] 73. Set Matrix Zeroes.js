48e145ea898a583bc5c7cc9080baf3d4e491410a
comt.mix@gmail.com <comt.mix@gmail.com>
2023-02-22 17:35:00 +0900

    setZeroes

--- problems/[230220] 73. Set Matrix Zeroes.js
+++ problems/[230220] 73. Set Matrix Zeroes.js
@@ -1,9 +1,29 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/set-matrix-zeroes
  *
- * @param {number[][]} matrix
- * @return {void} Do not return anything, modify matrix in-place instead.
- */
-const setZeroes = function(matrix) {
-    
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const setZeroes = function (matrix) {
+  let position = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) position.push([i, j]);
+    }
+  }
+
+  for (let i = 0; i < position.length; i++) {
+    let [x, y] = position[i];
+
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrix[x][j] = 0;
+    }
+
+    for (let j = 0; j < matrix.length; j++) {
+      matrix[j][y] = 0;
+    }
+  }
+
+  return matrix;
 };
