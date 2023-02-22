b291d4173e9bbc07f4155bb53dc0f049b1b505b7
tpdud406 <tpdud406@naver.com>
2023-02-22 17:23:51 +0900

    [230220] 73. Set Matrix Zeroes

--- problems/[230220] 73. Set Matrix Zeroes.js
+++ problems/[230220] 73. Set Matrix Zeroes.js
@@ -1,9 +1,35 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/set-matrix-zeroes
  *
- * @param {number[][]} matrix
- * @return {void} Do not return anything, modify matrix in-place instead.
- */
-const setZeroes = function(matrix) {
-    
+ * @param {number[][]} matrix//성공!
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const setZeroes = function (matrix) {
+  const XindexOfzeroValue = [];
+  const YindexOfzeroValue = new Set();
+
+  for (const [yIndex, row] of matrix.entries()) {
+    for (const [xIndex, cell] of row.entries()) {
+      if (cell === 0) {
+        XindexOfzeroValue.push(xIndex);
+        YindexOfzeroValue.add(yIndex);
+      }
+    }
+  }
+
+  const dupleRemoveYIndex = [...YindexOfzeroValue];
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[dupleRemoveYIndex[i]]) {
+      matrix[dupleRemoveYIndex[i]] = Array.from(
+        { length: matrix[0].length },
+        () => 0
+      );
+    }
+
+    for (let j = 0; j < XindexOfzeroValue.length; j++) {
+      matrix[i][XindexOfzeroValue[j]] = 0;
+    }
+  }
+
+  return matrix;
 };
