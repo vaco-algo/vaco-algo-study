daf9b8c24372a7041fcfcd325081e0d6ebadbe68
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-02-27 12:45:16 +0900

    solve

--- problems/[230227] 108. Convert Sorted Array to Binary Search Tree.js
+++ problems/[230227] 108. Convert Sorted Array to Binary Search Tree.js
@@ -12,5 +12,21 @@
  * @return {TreeNode}
  */
 const sortedArrayToBST = function(nums) {
+  if (nums.length === 1) {
+    return new TreeNode(nums[0]);
+  }
 
+  const pivotIndex = Math.floor(nums.length / 2);
+  const left = nums.length >= 2
+    ? sortedArrayToBST(nums.slice(0, pivotIndex))
+    : null;
+  const right = nums.length >= 3
+    ? sortedArrayToBST(nums.slice(pivotIndex + 1))
+    : null;
+
+  return new TreeNode(
+    nums[pivotIndex],
+    left,
+    right,
+  );
 };
