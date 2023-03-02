d486c944ab61359c959f41f66f83d0394d2818d3
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-03-02 12:38:49 +0900

    solve 2

--- problems/[230302] 101. Symmetric Tree.js
+++ problems/[230302] 101. Symmetric Tree.js
@@ -16,48 +16,49 @@ const isSymmetric = function(root) {
   if (!root?.left && !root?.right) {
     return true;
   }
-
+ 
   const queue = [root];
   let stack = [];
   let current = null;
-
+ 
   while (queue.length > 0) {
     current = queue.shift();
-
+ 
     current?.left && queue.push(current.left);
     current?.right && queue.push(current.right);
-
+ 
     if (!current?.left && !current?.right) {
       stack.push(false);
       while (
         stack.length > 1
         && stack[stack.length - 1] === stack[stack.length - 2]
-      ) {
+      ) { 
         stack.pop();
         stack.pop();
       }
-
+ 
       continue;
     }
-
+ 
     stack.push(current.left?.val);
     while (
       stack.length > 1
       && stack[stack.length - 1] === stack[stack.length - 2]
-    ) {
+    ) { 
       stack.pop();
       stack.pop();
     }
-
+ 
     stack.push(current.right?.val);
     while (
       stack.length > 1
       && stack[stack.length - 1] === stack[stack.length - 2]
-    ) {
+    ) { 
       stack.pop();
       stack.pop();
     }
   }
-
+ 
   return stack.length === 0;
 };
+ 
