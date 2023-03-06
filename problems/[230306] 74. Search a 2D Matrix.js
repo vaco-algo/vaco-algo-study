e855594c2706d840811725e4dd38e127bcfee6a6
taeuk <dcjimin@gmail.com>
2023-03-06 16:23:05 +0900

    solve 230306

--- problems/[230306] 74. Search a 2D Matrix.js
+++ problems/[230306] 74. Search a 2D Matrix.js
@@ -1,10 +1,23 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/search-a-2d-matrix
  *
- * @param {number[][]} matrix
- * @param {number} target
- * @return {boolean}
- */
-const searchMatrix = function(matrix, target) {
-    
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+const searchMatrix = function (matrix, target) {
+  for (const row of matrix) {
+    const first = row[0];
+    const last = row.at(-1);
+
+    if (target < first) return false;
+    if (target > last) continue;
+
+    for (const num of row) {
+      if (target === num) return true;
+      if (target < num) return false;
+    }
+  }
+
+  return false;
 };
