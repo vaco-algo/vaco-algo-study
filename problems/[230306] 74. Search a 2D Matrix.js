64cc36faaadb31c3b7c2b6f4f579ba5386d6ed92
taeuk <dcjimin@gmail.com>
2023-03-06 16:57:57 +0900

    fix 230306

--- problems/[230306] 74. Search a 2D Matrix.js
+++ problems/[230306] 74. Search a 2D Matrix.js
@@ -5,18 +5,60 @@
  * @param {number} target
  * @return {boolean}
  */
+// const searchMatrix = function (matrix, target) {
+//   for (const row of matrix) {
+//     const first = row[0];
+//     const last = row.at(-1);
+
+//     if (target < first) return false;
+//     if (target > last) continue;
+
+//     for (const num of row) {
+//       if (target === num) return true;
+//       if (target < num) return false;
+//     }
+//   }
+
+//   return false;
+// };
+
 const searchMatrix = function (matrix, target) {
-  for (const row of matrix) {
-    const first = row[0];
-    const last = row.at(-1);
+  let left = 0;
+  let right = matrix.length - 1;
 
-    if (target < first) return false;
-    if (target > last) continue;
+  while (left <= right) {
+    const mid = Math.floor(left + right);
+    const row = matrix[mid];
 
-    for (const num of row) {
-      if (target === num) return true;
-      if (target < num) return false;
+    if (row[0] > target) {
+      right = mid - 1;
+      continue;
     }
+
+    if (row.at(-1) < target) {
+      left = mid + 1;
+      continue;
+    }
+
+    left = 0;
+    right = row.length - 1;
+
+    while (left <= right) {
+      const mid = Math.floor(left + right);
+      const value = row[mid];
+
+      if (value === target) return true;
+      if (value < target) {
+        left = mid + 1;
+        continue;
+      }
+      if (value > target) {
+        right = mid - 1;
+        continue;
+      }
+    }
+
+    return false;
   }
 
   return false;
