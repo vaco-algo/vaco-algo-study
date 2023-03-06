0a314000ae6c35f390db0acbac48e5a1c7868ce6
comt.mix@gmail.com <comt.mix@gmail.com>
2023-03-06 16:48:34 +0900

    searchMatrix

--- problems/[230306] 74. Search a 2D Matrix.js
+++ problems/[230306] 74. Search a 2D Matrix.js
@@ -1,10 +1,21 @@
 /**
  * leetcode problem link: https://leetcode.com/problems/search-a-2d-matrix
  *
- * @param {number[][]} matrix
- * @param {number} target
- * @return {boolean}
- */
-const searchMatrix = function(matrix, target) {
-    
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+const searchMatrix = function (matrix, target) {
+  let row = 0;
+  let col = matrix[0].length - 1;
+
+  if (!matrix.length) return false;
+
+  while (row < matrix.length && col >= 0) {
+    if (matrix[row][col] === target) return true;
+    else if (matrix[row][col] > target) col--;
+    else row++;
+  }
+
+  return false;
 };
