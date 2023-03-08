916d34ca52b5b34736bfacf64f6a09fa23552144
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-03-08 14:31:30 +0900

    얍

--- problems/[230306] 74. Search a 2D Matrix.js
+++ problems/[230306] 74. Search a 2D Matrix.js
@@ -6,5 +6,25 @@
  * @return {boolean}
  */
 const searchMatrix = function(matrix, target) {
-    
+  if (target < matrix[0][0]) {
+    return false;
+  }
+
+  let index = 0;
+
+  while (
+    matrix.length > 1
+    && index < matrix.length - 1
+    && matrix[index + 1][0] <= target
+  ) {
+    index += 1;
+  }
+
+  for (const num of matrix[index]) {
+    if (num === target) {
+      return true;
+    }
+  }
+
+  return false;
 };
