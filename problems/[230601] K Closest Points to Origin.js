23887b4de9bea4847930ae039a34d72ec5fd30dc
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-06-01 11:45:30 +0900

    fail

--- problems/[230601] K Closest Points to Origin.js
+++ problems/[230601] K Closest Points to Origin.js
@@ -5,6 +5,33 @@
  * @param {number} k
  * @return {number[][]}
  */
-const kClosest = function(points, k) {
-    
+const kClosest = function (points, k) {
+  let answer = [];
+  let minDistance;
+
+  for (const point of points) {
+    const currentDistance = Math.floor(
+      Math.sqrt(point[0] ** 2 + point[1] ** 2)
+    );
+
+    if (minDistance === currentDistance) {
+      answer.push(point);
+    }
+
+    if (
+      !minDistance ||
+      Math.sqrt((k - minDistance) ** 2) > Math.sqrt((k - currentDistance) ** 2)
+    ) {
+      minDistance = currentDistance;
+
+      if (answer.length) {
+        answer = [point];
+        continue;
+      }
+
+      answer.push(point);
+    }
+  }
+
+  return answer;
 };
