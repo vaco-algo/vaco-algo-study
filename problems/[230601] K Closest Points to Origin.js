e08548ffbde80037bf70314dc14d5ff3e792c959
hweejae <hwejae25@gamil.com>
2023-06-01 11:59:48 +0900

    Closest

--- problems/[230601] K Closest Points to Origin.js
+++ problems/[230601] K Closest Points to Origin.js
@@ -6,5 +6,20 @@
  * @return {number[][]}
  */
 const kClosest = function(points, k) {
-    
+  const pointsInfo = {};
+
+  points.forEach((points) => {
+    const distance = points[0] ** 2 + points[1] ** 2;
+    pointsInfo[distance] = points;
+  });
+
+  const distanceAscending = Object.keys(pointsInfo).sort((a, b) => a - b);
+  const result = [];
+
+  while (result.length < k) {
+    const index = result.length;
+    result.push(pointsInfo[distanceAscending[index]]);
+  }
+
+  return result;
 };
