9d8dd382a525b97cc334ba367855e8396765b05e
Jisook93 <leejisook0718@gmail.com>
2023-06-01 11:46:50 +0900

    230601 algo

--- problems/[230601] K Closest Points to Origin.js
+++ problems/[230601] K Closest Points to Origin.js
@@ -5,6 +5,21 @@
  * @param {number} k
  * @return {number[][]}
  */
-const kClosest = function(points, k) {
-    
+const kClosest = function (points, k) {
+  const obj = {};
+  const answer = [];
+
+  points.forEach((point, index) => {
+    const length = Math.sqrt(Math.pow(point[0], 2) + Math.pow(point[1], 2));
+    obj[index] = length;
+  });
+
+  const lengthArrayWithIndex = Object.entries(obj);
+  const sortedLengthArray = lengthArrayWithIndex.sort((a, b) => a[1] - b[1]);
+  const answerIndexArray = sortedLengthArray.slice(0, k);
+  answerIndexArray.forEach((item) => {
+    answer.push(points[item[0]]);
+  });
+
+  return answer;
 };
