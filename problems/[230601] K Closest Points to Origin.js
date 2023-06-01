85efe19033621708cce16a53ddf59b889a92cc96
HyunJoonKang <steady.kang27@gmail.com>
2023-06-01 11:39:51 +0900

    K Closest Points to Origin 풀이

--- problems/[230601] K Closest Points to Origin.js
+++ problems/[230601] K Closest Points to Origin.js
@@ -5,6 +5,17 @@
  * @param {number} k
  * @return {number[][]}
  */
-const kClosest = function(points, k) {
-    
+const kClosest = function (points, k) {
+  const distance = [];
+
+  for (let i = 0; i < points.length; i += 1) {
+    distance.push(Math.sqrt(points[i][0] ** 2 + points[i][1] ** 2));
+  }
+
+  const indexArray = points
+    .map((_, index) => index)
+    .sort((a, b) => distance[a] - distance[b]);
+  const result = indexArray.map((index) => points[index]);
+
+  return result.slice(0, k);
 };
