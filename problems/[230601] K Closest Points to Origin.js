ab1de82e199cfff378dd00f23b03c4168e0f2fc3
lin01-cloud <lin01.dev@gmail.com>
2023-06-03 19:30:09 +0900

    K closest Points to Origin

--- problems/[230601] K Closest Points to Origin.js
+++ problems/[230601] K Closest Points to Origin.js
@@ -5,6 +5,15 @@
  * @param {number} k
  * @return {number[][]}
  */
-const kClosest = function(points, k) {
-    
+const kClosest = function (points, k) {
+  return points
+    .sort((point1, point2) => {
+      const point1Distance = point1[0] ** 2 + point1[1] ** 2;
+      const point2Distance = point2[0] ** 2 + point2[1] ** 2;
+
+      if (point1Distance > point2Distance) return 1;
+      if (point1Distance < point2Distance) return -1;
+      return 1;
+    })
+    .slice(0, k);
 };
