8e5025d635611a572816fdff86dfcf0890054379
pmjuu <jsi04049@gmail.com>
2023-06-05 14:23:44 +0900

    solve problems

--- problems/[230605] Sort Colors.js
+++ problems/[230605] Sort Colors.js
@@ -4,6 +4,12 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const sortColors = function(nums) {
-    
+const sortColors = function (nums) {
+  for (let i = 0; i < nums.length; i += 1) {
+    for (let j = i + 1; j < nums.length; j += 1) {
+      if (nums[i] > nums[j]) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+      }
+    }
+  }
 };
