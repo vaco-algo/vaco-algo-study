880fce4af2c00eb56568f77bfc42febec89191b3
Jisook93 <leejisook0718@gmail.com>
2023-06-05 16:12:01 +0900

    20230605 algo2

--- problems/[230605] Sort Colors.js
+++ problems/[230605] Sort Colors.js
@@ -4,6 +4,15 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const sortColors = function(nums) {
-    
+const sortColors = function (nums) {
+  for (let n = 0; n < nums.length - 1; n++) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const a = nums[i + 1];
+        nums[i + 1] = nums[i];
+        nums[i] = a;
+      }
+    }
+  }
+  return nums;
 };
