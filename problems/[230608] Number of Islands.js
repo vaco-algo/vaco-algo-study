bc838c1a77eb52f9615782fd8035e214858586c7
hweejae <hwejae25@gamil.com>
2023-06-08 12:09:12 +0900

    complete

--- problems/[230608] Number of Islands.js
+++ problems/[230608] Number of Islands.js
@@ -5,5 +5,35 @@
  * @return {number}
  */
 const numIslands = function(grid) {
-    
+  let island = 0;
+
+  grid.forEach((col, colIndex) => {
+    col.forEach((row, rowIndex) => {
+      if (row === "1") {
+        eraseNextLand(colIndex, rowIndex);
+        island += 1;
+      }
+    });
+  });
+
+  function eraseNextLand (col, row) {
+    if (grid[col + 1]?.[row] === "1") {
+      grid[col + 1][row] = 0;
+      eraseNextLand(col + 1, row)
+    }
+    if (grid[col]?.[row + 1] === "1") {
+      grid[col][row + 1] = 0;
+      eraseNextLand(col, row + 1)
+    }
+    if (grid[col - 1]?.[row] === "1") {
+      grid[col - 1][row] = 0;
+      eraseNextLand(col - 1, row)
+    }
+    if (grid[col]?.[row - 1] === "1") {
+      grid[col][row - 1] = 0;
+      eraseNextLand(col, row - 1)
+    }
+  }
+
+  return island;
 };
