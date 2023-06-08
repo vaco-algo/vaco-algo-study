15f47097df9eb4721e1261c29d01299fbb55817e
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-06-08 16:27:49 +0900

    timeout

--- problems/[230608] Number of Islands.js
+++ problems/[230608] Number of Islands.js
@@ -4,6 +4,38 @@
  * @param {character[][]} grid
  * @return {number}
  */
-const numIslands = function(grid) {
-    
+const numIslands = function (grid) {
+  let num = 0;
+
+  function DFS(x, y) {
+    if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+      return;
+    } else {
+      for (let i = x; i < grid.length; i++) {
+        for (let j = y; j < grid[i].length; j++) {
+          if (grid[x][y] === "0") return;
+
+          grid[x][y] = "0";
+
+          DFS(x - 1, y);
+          DFS(x + 1, y);
+          DFS(x, y - 1);
+          DFS(x, y + 1);
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let current = grid[i][j];
+
+      if (current === "1") {
+        DFS(i, j, num);
+        num++;
+      }
+    }
+  }
+
+  return num;
 };
