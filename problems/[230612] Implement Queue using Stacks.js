df876f8cf032abe4d6bfd807c7fc13c9461aebb3
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-06-12 11:27:40 +0900

    solve problem

--- problems/[230612] Implement Queue using Stacks.js
+++ problems/[230612] Implement Queue using Stacks.js
@@ -1,44 +1,45 @@
-
-const MyQueue = function() {
-    
+const MyQueue = function () {
+  this.innerStack = [];
+  this.outerStack = [];
 };
 
-/** 
+/**
  * @param {number} x
  * @return {void}
  */
-MyQueue.prototype.push = function(x) {
-    
-};
-
-/**
- * leetcode problem link: https://leetcode.com/problems/implement-queue-using-stacks
- *
- * @return {number}
- */
-MyQueue.prototype.pop = function() {
-    
+MyQueue.prototype.push = function (x) {
+  this.innerStack.push(x);
 };
 
 /**
  * @return {number}
  */
-MyQueue.prototype.peek = function() {
-    
+MyQueue.prototype.pop = function () {
+  if (this.outerStack.length > 0) {
+    return this.outerStack.pop();
+  }
+
+  const limit = this.innerStack.length - 1;
+
+  for (let i = 0; i < limit; i += 1) {
+    this.outerStack.push(this.innerStack.pop());
+  }
+
+  return this.innerStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  return this.outerStack.length > 0
+    ? this.outerStack[this.outerStack.length - 1]
+    : this.innerStack[0];
 };
 
 /**
  * @return {boolean}
  */
-MyQueue.prototype.empty = function() {
-    
+MyQueue.prototype.empty = function () {
+  return this.innerStack.length === 0 && this.outerStack.length === 0;
 };
-
-/** 
- * Your MyQueue object will be instantiated and called as such:
- * const obj = new MyQueue()
- * obj.push(x)
- * const param_2 = obj.pop()
- * const param_3 = obj.peek()
- * const param_4 = obj.empty()
- */
