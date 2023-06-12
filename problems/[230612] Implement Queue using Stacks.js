2a10627628185f85d8fb9fd4534c33707cba832b
lin01-cloud <lin01.dev@gmail.com>
2023-06-12 14:30:26 +0900

    232. Implement Queue using Stacks

--- problems/[230612] Implement Queue using Stacks.js
+++ problems/[230612] Implement Queue using Stacks.js
@@ -1,14 +1,14 @@
-
-const MyQueue = function() {
-    
+const MyQueue = function () {
+  this.stack1 = [];
+  this.stack2 = [];
 };
 
-/** 
+/**
  * @param {number} x
  * @return {void}
  */
-MyQueue.prototype.push = function(x) {
-    
+MyQueue.prototype.push = function (x) {
+  this.stack1.push(x);
 };
 
 /**
@@ -16,25 +16,37 @@ MyQueue.prototype.push = function(x) {
  *
  * @return {number}
  */
-MyQueue.prototype.pop = function() {
-    
+MyQueue.prototype.pop = function () {
+  if (this.stack2.length > 0) {
+    return this.stack2.pop();
+  }
+
+  while (this.stack1.length > 1) {
+    this.stack2[this.stack2.length] = this.stack1.pop();
+  }
+
+  return this.stack1.pop();
 };
 
 /**
  * @return {number}
  */
-MyQueue.prototype.peek = function() {
-    
+MyQueue.prototype.peek = function () {
+  if (this.stack2.length > 0) {
+    return this.stack2[this.stack2.length - 1];
+  }
+
+  return this.stack1[0];
 };
 
 /**
  * @return {boolean}
  */
-MyQueue.prototype.empty = function() {
-    
+MyQueue.prototype.empty = function () {
+  return this.stack1.length < 1 && this.stack2.length < 1;
 };
 
-/** 
+/**
  * Your MyQueue object will be instantiated and called as such:
  * const obj = new MyQueue()
  * obj.push(x)
