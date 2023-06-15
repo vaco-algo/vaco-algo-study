bcf5571a292a39e9fa3687045936509068eed93d
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-06-15 16:01:26 +0900

    timeout

--- problems/[230615] Symmetric Tree.js
+++ problems/[230615] Symmetric Tree.js
@@ -12,6 +12,39 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-const isSymmetric = function(root) {
-    
+
+function DFS(node, sum, store) {
+  if (!node) {
+    store.push(...sum);
+  } else {
+    DFS(node.left, [...sum, node.left ? node.left.val : null], store);
+    DFS(node.right, [...sum, node.right ? node.right.val : null], store);
+  }
+}
+
+function mirrorDFS(node, sum, store) {
+  if (!node) {
+    store.push(...sum);
+  } else {
+    mirrorDFS(node.right, [...sum, node.right ? node.right.val : null], store);
+    mirrorDFS(node.left, [...sum, node.left ? node.left.val : null], store);
+  }
+}
+
+const isSymmetric = function (root) {
+  if ((root.left && !root.right) || (root.right && !root.left)) {
+    return false;
+  }
+
+  if (!root.left && !root.right) {
+    return true;
+  }
+
+  const leftChecker = [];
+  const rightChecker = [];
+
+  DFS(root.left, [root.left.val], leftChecker);
+  mirrorDFS(root.right, [root.right.val], rightChecker);
+
+  return JSON.stringify(leftChecker) === JSON.stringify(rightChecker);
 };
