c5d29da7546b47bed726b1ad21b2ece4f45b25ba
hweejae <hwejae25@gamil.com>
2023-06-19 15:40:38 +0900

    solved

--- problems/[230619] Top K Frequent Words.js
+++ problems/[230619] Top K Frequent Words.js
@@ -6,5 +6,15 @@
  * @return {string[]}
  */
 const topKFrequent = function(words, k) {
-    
+  const map = {};
+
+  words.forEach((word) => {
+    if (map[word]) return map[word] += 1;
+      map[word] = 1;
+  });
+
+  return Object.keys(map).sort().sort((wordA, wordB) => {
+    return map[wordB] - map[wordA];
+  }).splice(0,k); 
+
 };
