a387001eb37cd690db12166332fc3d7168ded0e0
HyunJoonKang <steady.kang27@gmail.com>
2023-06-19 10:59:40 +0900

    topKFrequent

--- problems/[230619] Top K Frequent Words.js
+++ problems/[230619] Top K Frequent Words.js
@@ -5,6 +5,34 @@
  * @param {number} k
  * @return {string[]}
  */
-const topKFrequent = function(words, k) {
-    
+const topKFrequent = function (words, k) {
+  const map = new Map();
+
+  words.forEach((element) => {
+    if (!map.has(element)) {
+      map.set(element, 1);
+    } else {
+      map.set(element, map.get(element) + 1);
+    }
+  });
+
+  const mapArr = [...map];
+
+  mapArr.sort((a, b) => {
+    if (a[1] > b[1]) {
+      return -1;
+    } else if (a[1] < b[1]) {
+      return 1;
+    } else {
+      if (a[0] < b[0]) {
+        return -1;
+      } else if (a[0] > b[0]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  });
+
+  return mapArr.slice(0, k).map((element) => element[0]);
 };
