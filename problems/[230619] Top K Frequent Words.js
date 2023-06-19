a3a35d19b505493c7f2903400cbc0a91bd4ce74e
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-06-19 13:04:32 +0900

    mission complete

--- problems/[230619] Top K Frequent Words.js
+++ problems/[230619] Top K Frequent Words.js
@@ -5,6 +5,26 @@
  * @param {number} k
  * @return {string[]}
  */
-const topKFrequent = function(words, k) {
-    
+const topKFrequent = function (words, k) {
+  const counter = words.reduce((acc, cur) => {
+    acc[cur] = acc[cur] + 1 || 1;
+
+    return acc;
+  }, {});
+
+  const sortedCounter = Object.entries(counter).sort((a, b) => {
+    if (a[1] !== b[1]) {
+      return b[1] - a[1];
+    } else {
+      if (a[0] > b[0]) {
+        return 1;
+      } else if (a[0] < b[0]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  });
+
+  return sortedCounter.slice(0, k).map((s) => s[0]);
 };
