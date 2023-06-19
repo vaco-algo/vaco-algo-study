fd8b9f55a7a38c6a2b43528d52e9b04d4c563335
lin01-cloud <lin01.dev@gmail.com>
2023-06-19 12:04:15 +0900

    692. Top K Frequent Words

--- problems/[230619] Top K Frequent Words.js
+++ problems/[230619] Top K Frequent Words.js
@@ -5,6 +5,19 @@
  * @param {number} k
  * @return {string[]}
  */
-const topKFrequent = function(words, k) {
-    
+const topKFrequent = function (words, k) {
+  const frequency = {};
+  const result = [];
+
+  words.forEach((word) => {
+    frequency[word] =
+      frequency[word] === undefined ? 1 : (frequency[word] += 1);
+  });
+
+  Object.entries(frequency)
+    .sort()
+    .sort((a, b) => b[1] - a[1])
+    .forEach((word) => result.push(word[0]));
+
+  return result.slice(0, k);
 };
