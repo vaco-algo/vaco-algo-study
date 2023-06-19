b361c9f3f4f1313abae60e3ebf7ae08e0f01452a
Jisook93 <leejisook0718@gmail.com>
2023-06-19 12:20:53 +0900

    230619-algo

--- problems/[230619] Top K Frequent Words.js
+++ problems/[230619] Top K Frequent Words.js
@@ -5,6 +5,30 @@
  * @param {number} k
  * @return {string[]}
  */
-const topKFrequent = function(words, k) {
-    
+const topKFrequent = function (words, k) {
+  const wordFrequent = {};
+
+  words.forEach((word) => {
+    wordFrequent[word] ? (wordFrequent[word] += 1) : (wordFrequent[word] = 1);
+  });
+
+  const wordFrequentArray = Object.entries(wordFrequent);
+
+  wordFrequentArray
+    .sort((a, b) => {
+      return b[1] - a[1];
+    })
+    .sort((a, b) => {
+      if (a[1] === b[1]) {
+        return a[0] > b[0] ? 1 : -1;
+      }
+    });
+
+  const answer = wordFrequentArray
+    .map((word) => {
+      return word[0];
+    })
+    .splice(0, k);
+
+  return answer;
 };
