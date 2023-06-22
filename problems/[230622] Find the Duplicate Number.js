a0df863ee8b79066484de2d5ce378e6ad72a4c3d
HyunJoonKang <steady.kang27@gmail.com>
2023-06-22 14:57:57 +0900

    find the duplicate number

--- problems/[230622] Find the Duplicate Number.js
+++ problems/[230622] Find the Duplicate Number.js
@@ -4,6 +4,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findDuplicate = function(nums) {
-    
+const findDuplicate = function (nums) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i += 1) {
+    if (!map.has(nums[i])) {
+      map.set(nums[i], 1);
+    } else {
+      return nums[i];
+    }
+  }
 };
