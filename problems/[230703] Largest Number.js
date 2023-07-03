b2c1e6cce0a8f917f1ca0f11caeb9205a3c79301
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-07-03 14:29:07 +0900

    mission complete

--- problems/[230703] Largest Number.js
+++ problems/[230703] Largest Number.js
@@ -4,6 +4,25 @@
  * @param {number[]} nums
  * @return {string}
  */
-const largestNumber = function(nums) {
-    
+const largestNumber = function (nums) {
+  const strs = nums
+    .map((num) => num + "")
+    .sort((a, b) => {
+      if (a[0] === b[0] && a !== b) {
+        if (a + b < b + a) {
+          return 1;
+        } else {
+          return -1;
+        }
+      } else {
+        return b.localeCompare(a);
+      }
+    });
+
+  return strs.reduce((acc, cur) => {
+    if (acc[0] === "0" && cur === "0") {
+      return acc;
+    }
+    return acc + cur;
+  }, "");
 };
