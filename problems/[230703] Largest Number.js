6ec00a6313619f1c95e7c3fdf22d117a5a38476a
HyunJoonKang <steady.kang27@gmail.com>
2023-07-04 14:01:19 +0900

    Largest Number

--- problems/[230703] Largest Number.js
+++ problems/[230703] Largest Number.js
@@ -4,6 +4,14 @@
  * @param {number[]} nums
  * @return {string}
  */
-const largestNumber = function(nums) {
-    
+const largestNumber = function (nums) {
+  let result = "";
+
+  nums
+    .sort(function (a, b) {
+      return `${b}${a}` - `${a}${b}`;
+    })
+    .forEach((el) => (result += el));
+
+  return result[0] === "0" ? "0" : result;
 };
