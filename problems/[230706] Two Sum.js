c0c734dbb3fcdffb1feaa423da7003edd0b476f7
Jisook93 <leejisook0718@gmail.com>
2023-07-06 13:55:48 +0900

    230706_algo

--- problems/[230706] Two Sum.js
+++ problems/[230706] Two Sum.js
@@ -5,6 +5,12 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function(nums, target) {
-    
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const outputIndex = nums.indexOf(target - nums[i]);
+
+    if (outputIndex !== -1 && outputIndex !== i) {
+      return [i, outputIndex];
+    }
+  }
 };
