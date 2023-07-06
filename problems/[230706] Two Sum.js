723ea917722d05c44d382b6692ab6f3c4695aee7
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-07-06 22:23:39 +0900

    mission complete

--- problems/[230706] Two Sum.js
+++ problems/[230706] Two Sum.js
@@ -6,18 +6,18 @@
  * @return {number[]}
  */
 
-const twoSum = function (nums, target) {
-  const store = {};
+  const twoSum = function (nums, target) {
+    const store = {};
 
-  for (let i = 0; i < nums.length; i++) {
-    if (store[target - nums[i]]) {
-      return [store[target - nums[i]][0], i];
-    }
+    for (let i = 0; i < nums.length; i++) {
+      if (store[target - nums[i]]) {
+        return [store[target - nums[i]][0], i];
+      }
 
-    if (store[nums[i]]) {
-      store[nums[i]].push(i);
-    } else {
-      store[nums[i]] = [i];
+      if (store[nums[i]]) {
+        store[nums[i]].push(i);
+      } else {
+        store[nums[i]] = [i];
+      }
     }
-  }
-};
+  };
