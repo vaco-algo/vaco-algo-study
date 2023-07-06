edd4c1cc446d89b6569d207643452f43630de7c2
ssong-yi <devsong25@gmail.com>
2023-07-06 17:45:05 +0900

    solve problems

--- problems/[230706] Two Sum.js
+++ problems/[230706] Two Sum.js
@@ -5,6 +5,31 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = function(nums, target) {
-    
+const twoSum = function (nums, target) {
+  const sortedNums = nums.slice().sort((a, b) => a - b);
+  let left = 0;
+  let right = sortedNums.length - 1;
+
+  while (left !== right) {
+    const currentSum = sortedNums[left] + sortedNums[right];
+
+    if (currentSum === target) {
+      const sumResult = [sortedNums[left], sortedNums[right]];
+      const result = [];
+
+      for (let i = 0; i < nums.length; i++) {
+        if (sumResult.includes(nums[i])) {
+          result.push(i);
+        }
+      }
+
+      return [left, right];
+    }
+
+    if (currentSum > target) {
+      right -= 1;
+    } else {
+      left += 1;
+    }
+  }
 };
