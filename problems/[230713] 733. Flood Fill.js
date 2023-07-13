9d62ef7d94d5b66a08dfa62354148a504531c6a3
gunhwalee <ghlee2588@gmail.com>
2023-07-13 12:30:24 +0900

    DFS로 풀이

--- problems/[230713] 733. Flood Fill.js
+++ problems/[230713] 733. Flood Fill.js
@@ -9,6 +9,39 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, color) {
-    
+const floodFill = function (image, sr, sc, newColor) {
+  if (image[sr][sc] === newColor) return image;
+
+  const currentColor = image[sr][sc];
+
+  image[sr][sc] = newColor;
+  DFS(image, sr, sc, newColor, currentColor);
+
+  return image;
 };
+
+function DFS(grid, row, col, target, origin) {
+  const directions = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+
+  for (const direction of directions) {
+    const newRow = row + direction[0];
+    const newCol = col + direction[1];
+
+    if (
+      newRow < 0 ||
+      newCol < 0 ||
+      newRow >= grid.length ||
+      newCol >= grid[0].length ||
+      grid[newRow][newCol] !== origin
+    )
+      continue;
+
+    grid[newRow][newCol] = target;
+    DFS(grid, newRow, newCol, target, origin);
+  }
+}
