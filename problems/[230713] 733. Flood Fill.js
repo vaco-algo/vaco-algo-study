8070661bdd70b1d1982e62ef31b4143c2f787ff0
Jisook93 <leejisook0718@gmail.com>
2023-07-14 13:59:42 +0900

    230714_algo

--- problems/[230713] 733. Flood Fill.js
+++ problems/[230713] 733. Flood Fill.js
@@ -9,6 +9,22 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, color) {
-    
+var floodFill = function (image, sr, sc, color) {
+  const initialNum = image[sr][sc];
+  changeColor(image, sr, sc, initialNum, color);
+
+  return image;
+};
+
+const changeColor = (image, x, y, initialNum, color) => {
+  if (x < 0 || y < 0 || x >= image.length || y >= image[0].length) return;
+  if (image[x][y] !== initialNum) return;
+  if (image[x][y] === color) return;
+
+  image[x][y] = color;
+
+  changeColor(image, x + 1, y, initialNum, color);
+  changeColor(image, x - 1, y, initialNum, color);
+  changeColor(image, x, y + 1, initialNum, color);
+  changeColor(image, x, y - 1, initialNum, color);
 };
