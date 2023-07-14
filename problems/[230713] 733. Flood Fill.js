7e63a96ec20aa244db8477ce39df9ddc6146c10b
isinthesky <hijde@naver.com>
2023-07-14 15:07:40 +0900

    733. Flood Fill

--- problems/[230713] 733. Flood Fill.js
+++ problems/[230713] 733. Flood Fill.js
@@ -9,6 +9,27 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, color) {
-    
+
+var floodFill = function (image, sr, sc, color) {
+  curVal = image[sr][sc];
+  rowLength = image.length;
+  colLength = image[0].length;
+
+  dfs = (arr, row, col, val) => {
+    if (row < 0 || row >= rowLength) return;
+    if (col < 0 || col >= colLength) return;
+
+    if (arr[row][col] == val) return;
+    if (arr[row][col] != curVal) return;
+
+    arr[row][col] = val;
+
+    dfs(arr, row - 1, col, val);
+    dfs(arr, row + 1, col, val);
+    dfs(arr, row, col + 1, val);
+    dfs(arr, row, col - 1, val);
+  };
+
+  dfs(image, sr, sc, color);
+  return image;
 };
