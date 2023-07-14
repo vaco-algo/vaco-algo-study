313e9155b8af0e776ebe132dfe2a13ff1519de28
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-07-14 20:04:01 +0900

    풀이 완료

--- problems/[230713] 733. Flood Fill.js
+++ problems/[230713] 733. Flood Fill.js
@@ -9,6 +9,25 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, color) {
-    
+var floodFill = function (image, sr, sc, color) {
+  if (image[sr][sc] === color) return image;
+
+  const startColor = image[sr][sc];
+  const queue = [[sr, sc]];
+
+  while (queue.length > 0) {
+    const currentPixel = queue.shift();
+    const [i, j] = currentPixel;
+
+    if (image[i][j] === startColor) {
+      image[i][j] = color;
+
+      if (i > 0) queue.push([i - 1, j]);
+      if (j > 0) queue.push([i, j - 1]);
+      if (i < image.length - 1) queue.push([i + 1, j]);
+      if (j < image[0].length - 1) queue.push([i, j + 1]);
+    }
+  }
+
+  return image;
 };
