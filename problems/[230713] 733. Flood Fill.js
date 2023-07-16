66ff5f033e4efc04bdf38912e7a447e85b618508
lin01-cloud <lin01.dev@gmail.com>
2023-07-16 19:23:40 +0900

    733.Flood Fill

--- problems/[230713] 733. Flood Fill.js
+++ problems/[230713] 733. Flood Fill.js
@@ -9,6 +9,26 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, color) {
-    
+const floodFill = (image, sr, sc, newColor, color) => {
+  const firstColor = color === undefined ? image[sr][sc] : color;
+
+  if (
+    sr < 0 ||
+    sc < 0 ||
+    sr >= image.length ||
+    sc >= image[sr].length ||
+    image[sr][sc] !== firstColor ||
+    image[sr][sc] === newColor
+  ) {
+    return image;
+  }
+
+  image[sr][sc] = newColor;
+
+  floodFill(image, sr + 1, sc, newColor, firstColor);
+  floodFill(image, sr - 1, sc, newColor, firstColor);
+  floodFill(image, sr, sc + 1, newColor, firstColor);
+  floodFill(image, sr, sc - 1, newColor, firstColor);
+
+  return image;
 };
