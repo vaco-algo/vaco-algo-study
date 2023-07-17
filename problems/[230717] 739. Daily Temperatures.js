1f730d53e6e0999200ade0cc8388a0802210c429
임현정 | Alex Im <84281505+h-alex2@users.noreply.github.com>
2023-07-17 20:04:32 +0900

    mission complete

--- problems/[230717] 739. Daily Temperatures.js
+++ problems/[230717] 739. Daily Temperatures.js
@@ -6,6 +6,19 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    
+var dailyTemperatures = function (temperatures) {
+  const answer = Array.from({ length: temperatures.length }, () => 0);
+
+  for (let i = 0; i < temperatures.length; i++) {
+    let count = 0;
+
+    for (let j = i + 1; j < temperatures.length; j++) {
+      count++;
+      if (temperatures[i] < temperatures[j]) {
+        answer[i] = count;
+        break;
+      }
+    }
+  }
+  return answer;
 };
