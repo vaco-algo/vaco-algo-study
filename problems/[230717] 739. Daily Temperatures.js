91130b74b3c6f13b9138a46c1c17c13764740bb7
HyukE <ming2ki2@gmail.com>
2023-07-17 11:01:00 +0000

    [2023.07.17] 이상혁 739.Daily Temperatures

--- problems/[230717] 739. Daily Temperatures.js
+++ problems/[230717] 739. Daily Temperatures.js
@@ -6,6 +6,20 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    
+const dailyTemperatures = function (temperatures) {
+  const stack = [];
+  const result = new Array(temperatures.length).fill(0);
+
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length > 0 &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      let index = stack.pop();
+      result[index] = i - index;
+    }
+    stack.push(i);
+  }
+
+  return result;
 };
