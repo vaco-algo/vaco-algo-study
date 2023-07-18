f2402f22b0761072e6a6519f849b70a3339335f9
gunhwalee <ghlee2588@gmail.com>
2023-07-18 23:06:16 +0900

    daily temperatures

--- problems/[230717] 739. Daily Temperatures.js
+++ problems/[230717] 739. Daily Temperatures.js
@@ -6,6 +6,24 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    
+const dailyTemperatures = function (temperatures) {
+  const result = [];
+  result[temperatures.length - 1] = 0;
+
+  for (let i = 0; i < temperatures.length; i += 1) {
+    let count = 0;
+
+    for (let j = i + 1; j < temperatures.length; j += 1) {
+      count += 1;
+
+      if (temperatures[i] < temperatures[j]) {
+        result[i] = count;
+        break;
+      }
+
+      if (j === temperatures.length - 1) result[i] = 0;
+    }
+  }
+
+  return result;
 };
