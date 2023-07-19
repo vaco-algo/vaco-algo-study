9dc0c4bbc5adf1e1f3d3432c3814e3082af19391
Jisook93 <leejisook0718@gmail.com>
2023-07-19 11:25:38 +0900

    230717_algo

--- problems/[230717] 739. Daily Temperatures.js
+++ problems/[230717] 739. Daily Temperatures.js
@@ -6,6 +6,17 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    
+var dailyTemperatures = function (temperatures) {
+  const answer = new Array(temp.length).fill(0);
+
+  for (let i = 0; i < temp.length - 1; i++) {
+    for (let n = i + 1; n < temp.length; n++) {
+      if (temp[i] < temp[n]) {
+        answer[i] = n - i;
+        break;
+      }
+    }
+  }
+
+  return answer;
 };
