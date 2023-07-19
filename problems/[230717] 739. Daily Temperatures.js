25bc2109a13d8fc06be7a64aa4fd23f977516fa1
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-07-19 11:12:50 +0900

    풀이완료

--- problems/[230717] 739. Daily Temperatures.js
+++ problems/[230717] 739. Daily Temperatures.js
@@ -6,4 +6,17 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function (temperatures) {};
+var dailyTemperatures = function (temperatures) {
+  const result = new Array(temperatures.length).fill(0);
+
+  for (let i = 0; i < temperatures.length; i += 1) {
+    for (let j = i + 1; j < temperatures.length; j += 1) {
+      if (temperatures[j] > temperatures[i]) {
+        result[i] = j - i;
+        break;
+      }
+    }
+  }
+
+  return result;
+};
