115bb0879c0564b5a3f7d93e1dfa470e11f71ded
isinthesky <hijde@naver.com>
2023-07-19 17:40:34 +0900

    230713-floodfill

--- problems/[230713] 733. Flood Fill.js
+++ problems/[230713] 733. Flood Fill.js
@@ -11,26 +11,47 @@
  */
 
 var floodFill = function (image, sr, sc, color) {
-  curVal = image[sr][sc];
-  rowLength = image.length;
-  colLength = image[0].length;
+  if (image[sr][sc] === color) return image;
 
-  dfs = (arr, row, col, val) => {
-    if (row < 0 || row >= rowLength) return;
-    if (col < 0 || col >= colLength) return;
+  const startColor = image[sr][sc];
+  const queue = [[sr, sc]];
 
-    if (arr[row][col] == val) return;
-    if (arr[row][col] != curVal) return;
+  while (queue.length > 0) {
+    const currentPixel = queue.shift();
+    const [i, j] = currentPixel;
 
-    arr[row][col] = val;
+    if (image[i][j] === startColor) {
+      image[i][j] = color;
 
-    dfs(arr, row - 1, col, val);
-    dfs(arr, row + 1, col, val);
-    dfs(arr, row, col + 1, val);
-    dfs(arr, row, col - 1, val);
-  };
-
-  dfs(image, sr, sc, color);
+      if (i > 0) queue.push([i - 1, j]);
+      if (j > 0) queue.push([i, j - 1]);
+      if (i < image.length - 1) queue.push([i + 1, j]);
+      if (j < image[0].length - 1) queue.push([i, j + 1]);
+    }
+  }
 
   return image;
+
+  // curVal = image[sr][sc];
+  // rowLength = image.length;
+  // colLength = image[0].length;
+
+  // dfs = (arr, row, col, val) => {
+  //   if (row < 0 || row >= rowLength) return;
+  //   if (col < 0 || col >= colLength) return;
+
+  //   if (arr[row][col] == val) return;
+  //   if (arr[row][col] != curVal) return;
+
+  //   arr[row][col] = val;
+
+  //   dfs(arr, row - 1, col, val);
+  //   dfs(arr, row + 1, col, val);
+  //   dfs(arr, row, col + 1, val);
+  //   dfs(arr, row, col - 1, val);
+  // };
+
+  // dfs(image, sr, sc, color);
+
+  // return image;
 };
