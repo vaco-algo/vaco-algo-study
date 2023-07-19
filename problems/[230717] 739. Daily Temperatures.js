65e3c99f44fb566f45ba3a660ab9cb0d807995f9
isinthesky <hijde@naver.com>
2023-07-19 19:18:46 +0900

    230717-dailytemperatures

--- problems/[230717] 739. Daily Temperatures.js
+++ problems/[230717] 739. Daily Temperatures.js
@@ -6,6 +6,17 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    
+var dailyTemperatures = function (temperatures) {
+  let i = 0;
+  let j = 1;
+
+  const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  while (j < temperatures.length)
+    if (i <= j) arr[i] = j - i;
+    else if (i > j) j += 1;
+
+  console.log("dt", arr);
 };
+
+dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]);
