52cb13453d37fcb93a1b2d24bbe08c236f536d4f
Yejin <skyaiwn08@gmail.com>
2023-07-22 23:23:56 +0900

    Design Add and Search Words Data Structure

--- problems/[230720] 211. Design Add and Search Words Data Structure.js
+++ problems/[230720] 211. Design Add and Search Words Data Structure.js
@@ -2,28 +2,51 @@
  * leetcode problem link: https://leetcode.com/problems/design-add-and-search-words-data-structure
  */
 
-
-var WordDictionary = function() {
-    
+const WordDictionary = function () {
+  this.trie = {};
 };
 
-/** 
+/**
  * @param {string} word
  * @return {void}
  */
-WordDictionary.prototype.addWord = function(word) {
-    
+WordDictionary.prototype.addWord = function (word) {
+  let root = this.trie;
+
+  for (let i = 0; i < word.length; i++) {
+    if (!root[word[i]]) {
+      root[word[i]] = {};
+    }
+    root = root[word[i]];
+  }
+
+  root.isEnd = true;
 };
 
-/** 
+/**
  * @param {string} word
  * @return {boolean}
  */
-WordDictionary.prototype.search = function(word) {
-    
+WordDictionary.prototype.search = function (word) {
+  return this.dfs(word, 0, this.trie);
 };
 
-/** 
+WordDictionary.prototype.dfs = function (word, index, node) {
+  if (index === word.length) return node.isEnd === true;
+
+  if (word[index] === ".") {
+    for (const key in node) {
+      if (this.dfs(word, index + 1, node[key])) return true;
+    }
+  }
+
+  if (node[word[index]]) {
+    return this.dfs(word, index + 1, node[word[index]]);
+  }
+
+  return false;
+};
+/**
  * Your WordDictionary object will be instantiated and called as such:
  * var obj = new WordDictionary()
  * obj.addWord(word)
