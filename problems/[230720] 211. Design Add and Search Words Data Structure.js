741ad64146529d87d4d1dcd31d60525f93536880
gunhwalee <ghlee2588@gmail.com>
2023-07-22 16:19:21 +0900

    완료

--- problems/[230720] 211. Design Add and Search Words Data Structure.js
+++ problems/[230720] 211. Design Add and Search Words Data Structure.js
@@ -2,28 +2,55 @@
  * leetcode problem link: https://leetcode.com/problems/design-add-and-search-words-data-structure
  */
 
-
-var WordDictionary = function() {
-    
+const WordDictionary = function () {
+  this.root = {};
 };
 
-/** 
+/**
  * @param {string} word
  * @return {void}
  */
-WordDictionary.prototype.addWord = function(word) {
-    
+WordDictionary.prototype.addWord = function (word) {
+  let node = this.root;
+
+  for (const char of word) {
+    if (!node[char]) node[char] = {};
+
+    node = node[char];
+  }
+
+  node.isEnd = true;
 };
 
-/** 
+/**
  * @param {string} word
  * @return {boolean}
  */
-WordDictionary.prototype.search = function(word) {
-    
+WordDictionary.prototype.search = function (word) {
+  let node = this.root;
+
+  const searchTrie = (trie, depth) => {
+    if (depth === word.length) return trie.isEnd || false;
+
+    const char = word[depth];
+
+    if (char === ".") {
+      for (const children of Object.keys(trie)) {
+        if (searchTrie(trie[children], depth + 1)) return true;
+      }
+
+      return false;
+    } else {
+      if (!Object.hasOwn(trie, char)) return false;
+
+      return searchTrie(tire[char], depth + 1);
+    }
+  };
+
+  return searchTrie(node, 0);
 };
 
-/** 
+/**
  * Your WordDictionary object will be instantiated and called as such:
  * var obj = new WordDictionary()
  * obj.addWord(word)
