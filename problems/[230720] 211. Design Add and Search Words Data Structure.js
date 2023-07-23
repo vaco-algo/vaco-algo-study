6dac0c0c8d031f72350bd97a74c168889ad51d83
hweejae <hwejae25@gamil.com>
2023-07-23 20:39:42 +0900

    complete

--- problems/[230720] 211. Design Add and Search Words Data Structure.js
+++ problems/[230720] 211. Design Add and Search Words Data Structure.js
@@ -2,28 +2,44 @@
  * leetcode problem link: https://leetcode.com/problems/design-add-and-search-words-data-structure
  */
 
-
-var WordDictionary = function() {
-    
+var WordDictionary = function () {
+  this.wordCode = [];
 };
 
-/** 
+/**
  * @param {string} word
  * @return {void}
  */
-WordDictionary.prototype.addWord = function(word) {
-    
+WordDictionary.prototype.addWord = function (word) {
+  [...word].forEach((str) => this.wordCode.push(str.charCodeAt()));
 };
 
-/** 
+/**
  * @param {string} word
  * @return {boolean}
  */
-WordDictionary.prototype.search = function(word) {
-    
+WordDictionary.prototype.search = function (word) {
+  let result = false;
+  const codeArray = [...word].filter(
+    (str) => str.charCodeAt() > 96 && str.charCodeAt() < 123
+  );
+
+  for (let i = 0; i <= this.wordCode.length - codeArray.length; i += 1) {
+    const hasFind = codeArray.every((str, searchIndex) => {
+      const strCode = str.charCodeAt();
+      const dic = this.wordCode[i + searchIndex];
+      return strCode === dic;
+    });
+
+    if (hasFind) {
+      result = true;
+    }
+  }
+
+  return result;
 };
 
-/** 
+/**
  * Your WordDictionary object will be instantiated and called as such:
  * var obj = new WordDictionary()
  * obj.addWord(word)
