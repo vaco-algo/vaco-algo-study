069a6a8bd0afcbe074da46b397c3825cdf262127
HyukE <ming2ki2@gmail.com>
2023-07-25 21:38:45 +0900

    [2023.07.25] 이상혁 24. Swap Nodes in Pairs

--- problems/[230720] 211. Design Add and Search Words Data Structure.js
+++ problems/[230720] 211. Design Add and Search Words Data Structure.js
@@ -2,28 +2,21 @@
  * leetcode problem link: https://leetcode.com/problems/design-add-and-search-words-data-structure
  */
 
+var WordDictionary = function () {};
 
-var WordDictionary = function() {
-    
-};
-
-/** 
+/**
  * @param {string} word
  * @return {void}
  */
-WordDictionary.prototype.addWord = function(word) {
-    
-};
+WordDictionary.prototype.addWord = function (word) {};
 
-/** 
+/**
  * @param {string} word
  * @return {boolean}
  */
-WordDictionary.prototype.search = function(word) {
-    
-};
+WordDictionary.prototype.search = function (word) {};
 
-/** 
+/**
  * Your WordDictionary object will be instantiated and called as such:
  * var obj = new WordDictionary()
  * obj.addWord(word)
