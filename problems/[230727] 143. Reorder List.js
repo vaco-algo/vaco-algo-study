d319b040c9cc5e171df285fe0688c392856c8f04
Yejin <skyaiwn08@gmail.com>
2023-07-28 16:09:30 +0900

    Reorder list

--- problems/[230727] 143. Reorder List.js
+++ problems/[230727] 143. Reorder List.js
@@ -13,6 +13,47 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-var reorderList = function(head) {
-    
+const reorderList = function (head) {
+  let secondeNode = splitList(head);
+  secondeNode = reverseList(secondeNode);
+  mergeTwoList(head, secondeNode);
+
+  function splitList(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next && fast.next.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    const secondHalf = slow.next;
+    slow.next = null;
+
+    return secondHalf;
+  }
+
+  function reverseList(list) {
+    let current = list;
+    let prev = null;
+    let next = null;
+
+    while (current) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    return prev;
+  }
+
+  function mergeTwoList(list1, list2) {
+    while (list2) {
+      let temp = list1.next;
+      list1.next = list2;
+      list1 = list2;
+      list2 = temp;
+    }
+  }
 };
