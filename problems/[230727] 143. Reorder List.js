a13d17da697f59b2f6770786b4bd7fb0250c5cf0
Jisook93 <leejisook0718@gmail.com>
2023-07-28 16:16:46 +0900

    0728_algo

--- problems/[230727] 143. Reorder List.js
+++ problems/[230727] 143. Reorder List.js
@@ -13,6 +13,24 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-var reorderList = function(head) {
-    
+var reorderList = function (head) {
+  let node = head;
+  const stack = [];
+  while (node) {
+    stack.push(node);
+    node = node.next;
+  }
+
+  node = head;
+  const length = stack.length;
+
+  for (let i = 0; i < length; i++) {
+    if (i % 2 === 0) {
+      node.next = stack.shift();
+    } else {
+      node.next = stack.pop();
+    }
+    node = node.next;
+    node.next = null;
+  }
 };
