7c4a5547492c732d2f4c289d4231081b7d7ddfa4
HyunJoonKang <steady.kang27@gmail.com>
2023-07-31 02:01:51 +0900

    reorderList

--- problems/[230727] 143. Reorder List.js
+++ problems/[230727] 143. Reorder List.js
@@ -13,6 +13,34 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-var reorderList = function(head) {
-    
+const reorderList = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  let prev = null;
+  let cur = slow.next;
+
+  while (cur) {
+    let temp = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = temp;
+  }
+
+  slow.next = null;
+
+  let h1 = head;
+  let h2 = prev;
+
+  while (h2) {
+    let temp = h1.next;
+    h1.next = h2;
+    h1 = h2;
+    h2 = temp;
+  }
 };
