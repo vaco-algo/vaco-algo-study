b73c2e222e6b02385fdf2d0503d5d5965f938bde
isinthesky <hijde@naver.com>
2023-07-31 19:04:44 +0900

    reorderlist

--- problems/[230727] 143. Reorder List.js
+++ problems/[230727] 143. Reorder List.js
@@ -13,6 +13,34 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
-var reorderList = function(head) {
-    
+var reorderList = function (head) {
+  if (!head) return;
+  if (!head.next) return head;
+
+  const stack = [];
+  let node = head;
+  length = 0;
+
+  while (node) {
+    stack.push(node);
+    node = node.next;
+    length += 1;
+  }
+
+  node = head;
+
+  for (let i = 0; i < Math.floor(length / 2); i += 1) {
+    getNode = stack.pop();
+    nextNode = node.next;
+
+    node.next = getNode;
+    getNode.next = nextNode;
+    node = nextNode;
+  }
+
+  if (length % 2 > 0) {
+    node.next = null;
+  } else {
+    node.next.next = null;
+  }
 };
