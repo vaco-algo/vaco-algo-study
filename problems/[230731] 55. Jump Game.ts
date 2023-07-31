05fa4c315e1acd4e0d58f55a1e44755aaa0a38a7
gunhwalee <ghlee2588@gmail.com>
2023-07-31 16:04:50 +0900

    Jump Game 문제 생성

---
+++ problems/[230731] 55. Jump Game.ts
@@ -0,0 +1,12 @@
+/**
+ * leetcode problem link: https://leetcode.com/problems/jump-game/
+ */
+
+/**
+ * @param {number[]} nums each element in the array represents your maximum jump length at that position.
+ * @returns {boolean} Return true if you can reach the last index, or false otherwise.
+ */
+
+const canJump = (nums: number[]): boolean => {
+  return false;
+}
