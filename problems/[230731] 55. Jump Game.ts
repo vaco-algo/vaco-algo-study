489a7dc4b3f9245e4ae5fd3bbfc08d6e8f072dd8
gunhwalee <ghlee2588@gmail.com>
2023-08-02 17:14:05 +0900

    제출

--- problems/[230731] 55. Jump Game.ts
+++ problems/[230731] 55. Jump Game.ts
@@ -8,5 +8,18 @@
  */
 
 const canJump = (nums: number[]): boolean => {
-  return false;
+  if (nums.length === 1) return true;
+
+  let maxSteps: number = 0;
+  const target: number = nums.length - 1;
+
+  for (let i = 0; i < nums.length; i += 1) {
+    if (maxSteps <= i && nums[i] === 0) return false;
+
+    maxSteps = Math.max(maxSteps, nums[i] + i);
+
+    if (maxSteps >= target) return true;
+  }
+
+  return false
 }
