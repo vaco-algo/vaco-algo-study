5ebe8eafc559aed072fb54e675a0a94d2187f6c7
gunhwalee <ghlee2588@gmail.com>
2023-08-03 19:39:22 +0900

    제출

--- problems/[230803] 28. Find the Index of the First Occurrence in a String.ts
+++ problems/[230803] 28. Find the Index of the First Occurrence in a String.ts
@@ -8,5 +8,19 @@
  * @return {number}
  */
 const strStr = (haystack: string, needle: string): number => {
-  return 1;
+  let target: number = 0;
+  let index: number = 0;
+
+  while (target <= index && index < haystack.length) {
+    if (haystack[index] === needle[target]) {
+      if (target === needle.length - 1) return index - target;
+      target += 1;
+    } else {
+      index -= target;
+      target = 0;
+    }
+    index += 1;
+  }
+
+  return -1;
 };
