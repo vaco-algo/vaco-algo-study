15641d50be7d70931ba9421f80fbed2fcd3f523e
HyunJoonKang <steady.kang27@gmail.com>
2023-08-05 23:43:20 +0900

    Find the Index of the First Occurrence in a String

--- problems/[230803] 28. Find the Index of the First Occurrence in a String.js
+++ problems/[230803] 28. Find the Index of the First Occurrence in a String.js
@@ -7,6 +7,6 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    
+const strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
 };
