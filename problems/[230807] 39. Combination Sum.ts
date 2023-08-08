d58296e9b56ad870a4aa0a6b4c4e038db1503f99
gunhwalee <ghlee2588@gmail.com>
2023-08-08 19:30:02 +0900

    제출

--- problems/[230807] 39. Combination Sum.ts
+++ problems/[230807] 39. Combination Sum.ts
@@ -3,9 +3,30 @@
 */
 
 /**
- * @param {number[]} candidate
+ * @param {number[]} candidates
  * @param {number} target
- * @return {number[][]}
+ * @return {number[][]} a list of all unique combinations of candidates where the chosen numbers sum to target
  */
 
-const combinationSum = (candidate: number[], target: number): number[][] => {};
+const combinationSum = (candidates: number[], target: number): number[][] => {
+  const result: number[][] = [];
+
+  const DFS = (candidate: number[] = [], goal: number = target, index: number = 0): void => {
+    const sum: number = candidate.reduce((acc, cur) => acc + cur, 0);
+
+    if (sum === goal) {
+      result.push(candidate);
+      return;
+    }
+
+    for (let i = index; i < candidates.length; i += 1) {
+      if (sum + candidates[i] <= goal) {
+        DFS([...candidate, candidates[i]], goal, i);
+      }
+    }
+  };
+
+  DFS();
+
+  return result;
+};
