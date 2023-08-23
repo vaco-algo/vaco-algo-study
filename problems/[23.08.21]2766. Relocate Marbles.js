cbeb01549ee7234d2a6644d6753dca2010d9423b
HyukE <ming2ki2@gmail.com>
2023-08-23 15:43:01 +0900

    [23.08.21]2766. Relocate Marbles

--- problems/[23.08.21]2766. Relocate Marbles.js
+++ problems/[23.08.21]2766. Relocate Marbles.js
@@ -6,14 +6,14 @@
  */
 //시간복잡도 O(n*m) n = nums의 길이, m = moveFrom, moveTo의 길이
 const relocateMarbles = function (nums, moveFrom, moveTo) {
-  let result = [...nums];
+  const result = [...nums];
 
   for (let i = 0; i < moveFrom.length; i++) {
-    if (result.indexOf(moveFrom[i]) !== -1) {
-      let resultIndex = result.indexOf(moveFrom[i]);
+    while (result.indexOf(moveFrom[i]) !== -1) {
+      const resultIndex = result.indexOf(moveFrom[i]);
       result[resultIndex] = moveTo[i];
     }
   }
 
-  return result.sort();
+  return [...new Set(result)].sort((a, b) => a - b);
 };
