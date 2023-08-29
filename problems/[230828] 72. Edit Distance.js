5a6634a339713a6de77e8cb79eff94b7d8e9615c
gunhwalee <ghlee2588@gmail.com>
2023-08-29 12:44:32 +0900

    문제 생성

---
+++ problems/[230828] 72. Edit Distance.js
@@ -0,0 +1,10 @@
+/**
+ * leetcode problem link: https://leetcode.com/problems/edit-distance/
+ */
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+const minDistance = (word1, word2) => {};
