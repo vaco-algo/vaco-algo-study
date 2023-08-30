7ce1024d69595ec2aab0b78c91ecd4cea3267fd7
gunhwalee <ghlee2588@gmail.com>
2023-08-30 12:59:08 +0900

    제출

--- problems/[230828] 72. Edit Distance.js
+++ problems/[230828] 72. Edit Distance.js
@@ -7,4 +7,32 @@
  * @param {string} word2
  * @return {number}
  */
-const minDistance = (word1, word2) => {};
+const minDistance = (word1, word2) => {
+  const dp = new Array(word1.length + 1)
+    .fill(0)
+    .map(() => new Array(word2.length + 1).fill(0));
+
+  for (let i = 0; i <= word1.length; i++) {
+    dp[i][0] = i;
+  }
+
+  for (let i = 0; i <= word2.length; i++) {
+    dp[0][i] = i;
+  }
+
+  for (let i = 1; i <= word1.length; i++) {
+    for (let j = 1; j <= word2.length; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(
+          dp[i - 1][j] + 1,
+          dp[i][j - 1] + 1,
+          dp[i - 1][j - 1] + 1
+        );
+      }
+    }
+  }
+
+  return dp[word1.length][word2.length];
+};
