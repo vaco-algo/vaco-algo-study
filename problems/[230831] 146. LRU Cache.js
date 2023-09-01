65a1468f431ffe34a5bd5c9c1343b305db8dcb00
gunhwalee <ghlee2588@gmail.com>
2023-09-01 13:37:33 +0900

    제출

--- problems/[230831] 146. LRU Cache.js
+++ problems/[230831] 146. LRU Cache.js
@@ -5,28 +5,42 @@
 /**
  * @param {number} capacity
  */
-var LRUCache = function(capacity) {
-    
+var LRUCache = function (capacity) {
+  this.cache = new Map();
+  this.size = capacity;
 };
 
-/** 
+/**
  * @param {number} key
  * @return {number}
  */
-LRUCache.prototype.get = function(key) {
-    
+LRUCache.prototype.get = function (key) {
+  if (!this.cache.has(key)) return -1;
+
+  const value = this.cache.get(key);
+  this.cache.delete(key);
+  this.cache.set(key, value);
+
+  return this.cache.get(key);
 };
 
-/** 
- * @param {number} key 
+/**
+ * @param {number} key
  * @param {number} value
  * @return {void}
  */
-LRUCache.prototype.put = function(key, value) {
-    
+LRUCache.prototype.put = function (key, value) {
+  if (this.cache.has(key)) this.cache.delete(key);
+
+  this.cache.set(key, value);
+
+  if (this.cache.size > this.size) {
+    const first = this.cache.keys().next().value;
+    this.cache.delete(first);
+  }
 };
 
-/** 
+/**
  * Your LRUCache object will be instantiated and called as such:
  * var obj = new LRUCache(capacity)
  * var param_1 = obj.get(key)
