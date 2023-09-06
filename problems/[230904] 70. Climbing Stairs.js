f4b25b9d136b260a9de82e1cf2d399b9cad0c29c
HyunJoonKang <steady.kang27@gmail.com>
2023-09-06 23:17:08 +0900

    Climbing Stairs

--- problems/[230904] 70. Climbing Stairs.js
+++ problems/[230904] 70. Climbing Stairs.js
@@ -6,19 +6,20 @@
  * @param {number} n
  * @return {number}
  */
-const climbStairs = function (n) {
-  // 풀아 방법: 중복되는 경우의 수가 있을것 같으므로 DP를 사용해봐야겠다. 또는 피보나치 수열과 비슷해보인다.
-  // 시간 복잡도: O(n)
-  // 공간 복잡도: O(n)
-
-  const dp = new Array(45);
-
-  dp[1] = 1;
-  dp[2] = 2;
-
-  for (let i = 3; i <= n; i += 1) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-  }
-
-  return dp[n];
-};
+ const climbStairs = function (n) {
+   // 풀아 방법: 중복되는 경우의 수가 있을것 같으므로 DP를 사용해봐야겠다. 또는 피보나치 수열과 비슷해보인다.
+   // 시간 복잡도: O(n)
+   // 공간 복잡도: O(n)
+ 
+   const dp = new Array(45);
+ 
+   dp[1] = 1;
+   dp[2] = 2;
+ 
+   for (let i = 3; i <= n; i += 1) {
+     dp[i] = dp[i - 1] + dp[i - 2];
+   }
+ 
+   return dp[n];
+ };
+ 
