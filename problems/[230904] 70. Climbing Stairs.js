a86d14255169c42a56a6b8bdeb953aa937ea2bdc
gunhwalee <ghlee2588@gmail.com>
2023-09-06 17:12:07 +0900

    제출

--- problems/[230904] 70. Climbing Stairs.js
+++ problems/[230904] 70. Climbing Stairs.js
@@ -6,4 +6,13 @@
  * @param {number} n
  * @return {number}
  */
-const climbStairs = (n) => {};
+const climbStairs = function (n) {
+  const dp = new Array(n + 1).fill(0);
+
+  for (let i = 0; i < dp.length; i++) {
+    if (i < 2) dp[i] = 1;
+    else dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp.at(-1);
+};
