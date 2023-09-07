08dc039ac02a36711221868dcf6e3ac384e68a14
gunhwalee <ghlee2588@gmail.com>
2023-09-07 11:47:01 +0900

    문제 생성

---
+++ problems/[230907] 92. Reverse Linked List ||.js
@@ -0,0 +1,18 @@
+/**
+ * leetcode problem link: https://leetcode.com/problems/reverse-linked-list-ii/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+const reverseBetween = function (head, left, right) {};
