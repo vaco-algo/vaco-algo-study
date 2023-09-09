0b587b77c10182b3ee1810afcaab4925c3cf60ed
gunhwalee <ghlee2588@gmail.com>
2023-09-09 14:09:03 +0900

    제출

--- problems/[230907] 92. Reverse Linked List ||.js
+++ problems/[230907] 92. Reverse Linked List ||.js
@@ -15,4 +15,33 @@
  * @param {number} right
  * @return {ListNode}
  */
-const reverseBetween = function (head, left, right) {};
+const reverseBetween = function (head, left, right) {
+  if (!head || left === right) return head;
+
+  let start = head,
+    curr = head;
+  let count = 1;
+
+  while (count < left) {
+    start = curr;
+    curr = curr.next;
+    count++;
+  }
+
+  let reversed = null,
+    tail = curr;
+
+  while (count >= left && count <= right) {
+    const next = curr.next;
+    curr.next = reversed;
+    reversed = curr;
+    curr = next;
+
+    count++;
+  }
+
+  start.next = reversed;
+  tail.next = curr;
+
+  return left > 1 ? head : reversed;
+};
