aabb87731fffef0a7152583f7cce9fc1d3f782cf
공재혁 <ruud091@gmail.com>
2023-09-11 09:55:01 +0900

    (auto upload) Course Schedule

---
+++ problems/[230911] 207. Course Schedule.js
@@ -0,0 +1,12 @@
+/**
+ * leetcode problem link: https://leetcode.com/problems/course-schedule
+ */
+
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    
+};
