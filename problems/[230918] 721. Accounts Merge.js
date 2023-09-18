279dfc29300903cc614818aea0d738021d05aafc
공재혁 <ruud091@gmail.com>
2023-09-18 09:55:00 +0900

    (auto upload) Accounts Merge

---
+++ problems/[230918] 721. Accounts Merge.js
@@ -0,0 +1,11 @@
+/**
+ * leetcode problem link: https://leetcode.com/problems/accounts-merge
+ */
+
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function(accounts) {
+    
+};
