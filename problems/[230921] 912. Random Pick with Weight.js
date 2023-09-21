be64d5e408d06e59fc46b7bff9f61a4d72d457ee
공재혁 <ruud091@gmail.com>
2023-09-21 09:55:00 +0900

    (auto upload) Random Pick with Weight

---
+++ problems/[230921] 912. Random Pick with Weight.js
@@ -0,0 +1,23 @@
+/**
+ * leetcode problem link: https://leetcode.com/problems/random-pick-with-weight
+ */
+
+/**
+ * @param {number[]} w
+ */
+var Solution = function(w) {
+    
+};
+
+/**
+ * @return {number}
+ */
+Solution.prototype.pickIndex = function() {
+    
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(w)
+ * var param_1 = obj.pickIndex()
+ */
