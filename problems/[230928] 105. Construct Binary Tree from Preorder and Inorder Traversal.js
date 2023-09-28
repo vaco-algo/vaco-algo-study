2806f159196c4fc506940ae0ac6779e1389f743b
공재혁 <ruud091@gmail.com>
2023-09-28 09:55:01 +0900

    (auto upload) Construct Binary Tree from Preorder and Inorder Traversal

---
+++ problems/[230928] 105. Construct Binary Tree from Preorder and Inorder Traversal.js
@@ -0,0 +1,20 @@
+/**
+ * leetcode problem link: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    
+};
