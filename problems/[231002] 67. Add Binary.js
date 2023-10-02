e5f8843638973dae97eeb85329395aee09ea89ec
공재혁 <ruud091@gmail.com>
2023-10-02 09:55:01 +0900

    (auto upload) Add Binary

---
+++ problems/[231002] 67. Add Binary.js
@@ -0,0 +1,12 @@
+/**
+ * leetcode problem link: https://leetcode.com/problems/add-binary
+ */
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    
+};
